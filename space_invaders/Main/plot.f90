500111b7901fa0e9870a3ee7652f120a702d3a9d
arrigo <arrigo@0e440500-e291-4b27-a0db-898e6d94e59d>
2004-04-15 16:14:14 +0000

    PPPLOOOTTTTT !!!! ora funzia carlo
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@17 0e440500-e291-4b27-a0db-898e6d94e59d

--- space_invaders/Main/plot.f90
+++ space_invaders/Main/plot.f90
@@ -21,7 +21,6 @@
       INTEGER :: nx, ny, nz, nw1, nw2, nw, l
       INTEGER :: nx2, ny2, nz2, nii
       INTEGER :: nkp, nb, m, nsp
-      INTEGER :: ngptar(3)
       INTEGER :: ngptwann(3)
       INTEGER :: n, j, nrx, nry, ni
       INTEGER :: i, nmod, nt, nlim
@@ -41,20 +40,26 @@
       REAL(dbl), ALLOCATABLE :: vkpt(:,:)         ! vkpt(3,nkpts)
 
       REAL(dbl) :: scalf, tmaxx, tmax, ratmax, ratio
-      REAL(dbl) :: poscar( 3, ngrid+1, ngrid+1 )
-      REAL(dbl) :: posrel( 3, ngrid+1, ngrid+1 )
-      REAL(dbl) :: gridx, gridzx, gridy, gridzy
       REAL(dbl) :: dirc( 3, 3 ), recc( 3, 3 ), dirl( 3, 3 )
-      REAL(dbl) :: x
+      REAL(dbl) :: pos( 3 )
       REAL(dbl) :: gxx, gyy, gzz, testcubic
       REAL(dbl) :: x_0ang, y_0ang, z_0ang 
-      REAL(dbl) :: poscartbig( 3, natx*ngt, npsx )
+      REAL(dbl), ALLOCATABLE :: poscarwin( :, :, : )
       REAL(dbl) :: posion( 3, natx, npsx )
+      CHARACTER( LEN=2 ) :: nameat( npsx )
+      INTEGER :: indat( npsx )
+      INTEGER :: natwin( npsx )
 
       CHARACTER( LEN=11 ) :: frfft
       CHARACTER( LEN=11 ) :: fifft
 
       INTEGER :: dimwann, mxddim
+      INTEGER :: nrxl, nryl, nrzl
+      INTEGER :: nrxh, nryh, nrzh
+      INTEGER :: nrxd, nryd, nrzd
+      INTEGER :: nnrx, nnry, nnrz
+      LOGICAL :: okp( 3 )
+      REAL(dbl) :: aa
 
       PARAMETER ( citpi = ( 0.0d0, twopi) )
 
@@ -80,7 +85,7 @@
       END DO
 
       DO nsp = 1, nspec
-        READ(21) nionsp(nsp)
+        READ(21) nionsp(nsp), nameat(nsp)
       END DO
 
       DO nsp = 1, nspec
@@ -112,11 +117,6 @@
 
       CLOSE(21)
 
-!     PARAMETER ( nplwv = ngx*ngy*ngz, mplwv=ngx*ngy*(ngz+1) )
-!     PARAMETER ( iplwv = ( ngt*ngx ) * ( ngt*ngy ) * ( ngt*ngz+1) )
-!     PARAMETER ( jplwv = ( ngt*ngx ) * ( ngt*ngy ) * ( ngt*ngz ) ) 
-!     PARAMETER ( nionbig = nions * ngt * ngt * ngt)
-
       OPEN( 29, FILE='unitary.dat', STATUS='OLD', FORM='UNFORMATTED' )
 
       ALLOCATE( cu( nbands, nbands, nkpts ) )
@@ -126,14 +126,36 @@
 
 ! ... Initialize the data used for the fast fourier transforms
 
-      ngptar(1) = ngx
-      ngptar(2) = ngy
-      ngptar(3) = ngz
+      READ(5,*) nwann, nrxl, nrxh, nryl, nryh, nrzl, nrzh
+      PRINT *, 'plotting WF n. ', nwann
+      PRINT *, 'plot grid      ', nrxl, nrxh, nryl, nryh, nrzl, nrzh
+      PRINT *, 'total grid     ', 1, ngx, 1, ngy, 1, ngz
 
-      READ(5,*) nwann
-      PRINT*, 'plotting WF n. ',nwann
+      nrxd = ( nrxh - nrxl + 1 )
+      nryd = ( nryh - nryl + 1 ) 
+      nrzd = ( nrzh - nrzl + 1 )
 
-      ALLOCATE ( cwann( -ngx:ngs*ngx-1, -ngy:ngs*ngy-1, -ngz:ngs*ngz-1 ) ) 
+      nnrx = ABS( nrxl / ngx ) + 2
+      nnry = ABS( nryl / ngy ) + 2
+      nnrz = ABS( nrzl / ngz ) + 2
+
+      IF( nrxd < 1 ) THEN
+        WRITE( *, * ) ' nrxl, nrxh = ', nrxl, nrxh
+        CALL errore( ' plot ', ' wrong nrxl and nrxh ', 1 )
+      END IF
+
+      IF( nryd < 1 ) THEN
+        WRITE( *, * ) ' nryl, nryh = ', nryl, nryh
+        CALL errore( ' plot ', ' wrong nryl and nryh ', 1 )
+      END IF
+
+      IF( nrzd < 1 ) THEN
+        WRITE( *, * ) ' nrzl, nrzh = ', nrzl, nrzh
+        CALL errore( ' plot ', ' wrong nrzl and nrzh ', 1 )
+      END IF
+      
+
+      ALLOCATE ( cwann( nrxl:nrxh, nryl:nryh, nrzl:nrzh ) ) 
       cwann = ( 0.0d0, 0.0d0 )
 !
       ALLOCATE( cptwr(mplwv) )
@@ -151,35 +173,30 @@
             cptwr( nindpw(m,nkp) ) = cptwfp( m, nb, nkp )
           END DO
  
-          CALL cfft3d( cptwr, ngptar(1), ngptar(2), ngptar(3), ngptar(1), ngptar(2), ngptar(3), -1 )
+          CALL cfft3d( cptwr, ngx, ngy, ngz, ngx, ngy, ngz, -1 )
 
           DO m = 1, mplwv
-            cptwr(m) = conjg(cptwr(m)) * ngptar(1) * ngptar(2) * ngptar(3)
+            cptwr(m) = conjg(cptwr(m)) * ngx * ngy * ngz
           END DO
 
-          DO nzz = -ngz, ngs*ngz-1
-            nz = MOD(nzz,ngz)
-            IF( nz < 1 ) nz = nz + ngz
-            DO nyy = -ngy, ngs*ngy-1
-              ny = MOD( nyy, ngy )
-              IF ( ny < 1 ) ny = ny + ngy
-              DO nxx = -ngx, ngs*ngx-1
-                nx = MOD( nxx, ngx )
-                IF ( nx < 1 ) nx = nx + ngx
+          DO nzz = nrzl, nrzh
+            nz = MOD( nzz + nnrz * ngz , ngz ) + 1
+            DO nyy = nryl, nryh
+              ny = MOD( nyy + nnry * ngy , ngy ) + 1
+              DO nxx = nrxl, nrxh
+                nx = MOD( nxx + nnrx * ngx , ngx ) + 1
 
-                scalf = vkpt(1,nkp) * DBLE(nxx-1) / DBLE(ngx) + &
-                        vkpt(2,nkp) * DBLE(nyy-1) / DBLE(ngy) + &
-                        vkpt(3,nkp) * DBLE(nzz-1) / DBLE(ngz)
                 npoint = nx + (ny-1) * ngx + (nz-1) * ngy * ngx
-                catmp = EXP( citpi*scalf ) * cptwr(npoint) * cu(nb,nwann,nkp)
-                IF ( scalf /= 0.0d0 ) THEN
-                ELSE
-                 continue
-                ENDIF
+
+                scalf = vkpt(1,nkp) * DBLE(nxx) / DBLE(ngx) + &
+                        vkpt(2,nkp) * DBLE(nyy) / DBLE(ngy) + &
+                        vkpt(3,nkp) * DBLE(nzz) / DBLE(ngz)
+
+                catmp = EXP( citpi * scalf ) * cptwr( npoint ) * cu( nb, nwann, nkp )
  
 ! ...           Here it increments the wannier funcion in nx,ny,nz with the current nb,nkp bloch orbitals 
 
-                cwann(nxx,nyy,nzz) = cwann(nxx,nyy,nzz) + catmp
+                cwann( nxx, nyy, nzz ) = cwann( nxx, nyy, nzz ) + catmp
               END DO
             END DO
           END DO
@@ -192,105 +209,80 @@
       tmaxx = 0.0d0
       cmod = ( 1.0d0, 0.0d0 )
 
-      DO nzz = -ngz, ngs*ngz-1
-        DO nyy = -ngy, ngs*ngy-1
-          DO nxx= -ngx, ngs*ngx-1
-            cwann(nxx,nyy,nzz) = cwann(nxx,nyy,nzz) / DBLE(nkpts)
-            tmax = cwann(nxx,nyy,nzz) * CONJG(cwann(nxx,nyy,nzz))
+      DO nzz = nrzl, nrzh
+        DO nyy = nryl, nryh
+          DO nxx= nrxl, nrxh
+            cwann( nxx, nyy, nzz ) = cwann( nxx, nyy, nzz ) / DBLE(nkpts)
+            tmax = cwann( nxx, nyy, nzz ) * CONJG( cwann( nxx, nyy, nzz ) )
             IF ( tmax > tmaxx ) THEN
               tmaxx = tmax
-              cmod = cwann(nxx,nyy,nzz)
+              cmod = cwann( nxx, nyy, nzz )
             END IF
           END DO
         END DO
       END DO
 
       cmod = cmod / SQRT( REAL( cmod )**2 + AIMAG ( cmod )**2 )
-      DO nzz = -ngz, ngs*ngz-1
-        DO nyy = -ngy, ngs*ngy-1
-          DO nxx = -ngx, ngs*ngx-1
+      DO nzz = nrzl, nrzh
+        DO nyy = nryl, nryh
+          DO nxx = nrxl, nryh
             cwann(nxx,nyy,nzz) = cwann(nxx,nyy,nzz) / cmod
           END DO
         END DO
       END DO
-
-! ... Now define the cartesian grid of points poscar, that is going to be  plotted 
-! ... This is to be made more user-friendly
-
-      DO nry = 1, ngrid+1
-        DO nrx = 1, ngrid+1
-          poscar(1,nrx,nry) = 10.0d0 * DBLE(nrx-1) / DBLE(ngrid)
-          poscar(2,nrx,nry) = 10.0d0 * DBLE(nry-1) / DBLE(ngrid)
-          poscar(3,nrx,nry) = 0.0d0
-       END DO
-      END DO
-
-      gridx = 10.0d0
-      gridzx = 0.0d0
-      gridy = 10.0d0
-      gridzy = 0.0d0
-
-! ... And pass it into reciprocal coordinates 
-      DO nrx = 1, ngrid+1
-        DO nry = 1, ngrid+1
-          DO m = 1, 3
-            posrel( m, nrx, nry ) = 0.0d0
-            DO j = 1, 3
-              posrel(m,nrx,nry) = posrel(m,nrx,nry) + poscar(j,nrx,nry) * recc(m,j)
-            END DO
-            posrel(m,nrx,nry) = posrel(m,nrx,nry) / twopi
-            posrel(m,nrx,nry) = ( posrel(m,nrx,nry) + 1.0d0 ) / DBLE(ngt)
-          END DO
-        END DO
-      END DO
-
-! ... Now pass the wannier functions in reciprocal space, to do the Fourier interpolation
-      iplwv = ( ngt*ngx ) * ( ngt*ngy ) * ( ngt*ngz+1)
-      jplwv = ( ngt*ngx ) * ( ngt*ngy ) * ( ngt*ngz )
-
-      ngptwann(1) = ngt * ngx
-      ngptwann(2) = ngt * ngy
-      ngptwann(3) = ngt * ngz
-
-
 
 ! ... We prepare a graphic output for gopenmol or for dan, depending on the geometry of the cell; 
 !     if it is orthorombic, gopenmol is used (this includes the cubic case); otherwise, dan is used.
 
 ! ... This is to create the WF...gau output, to be read by gOpenMol (coordinates + isosurfaces)
 
+      natwin = 0
       DO nsp = 1, nspec
-        DO nii = 1, nionsp(nsp)
-          DO nx = -1, ngs-1
-            DO ny = -1, ngs-1
-              DO nz = -1, ngs-1
-                ni = ( 1 + (nii-1) * ngt**3 ) + (nx+1) * ngt * ngt + (ny+1) * ngt + nz + 1
-                DO m = 1, 3
-                  poscartbig(m,ni,nsp) = &
-                  ( posion(1,nii,nsp) + DBLE(nx) ) * dirc(1,m) + &
-                  ( posion(2,nii,nsp) + DBLE(ny) ) * dirc(2,m) + &
-                  ( posion(3,nii,nsp) + DBLE(nz) ) * dirc(3,m)
-                  poscartbig(m,ni,nsp) = poscartbig(m,ni,nsp) / bohr
-                END DO
+        DO ni = 1, nionsp(nsp)
+          DO nx = -2, 2
+            DO ny = -2, 2
+              DO nz = -2, 2
+                pos(1) = ( posion(1,ni,nsp) + DBLE(nx) ) * ngx
+                pos(2) = ( posion(2,ni,nsp) + DBLE(ny) ) * ngy
+                pos(3) = ( posion(3,ni,nsp) + DBLE(nz) ) * ngz
+                okp(1) = ( pos(1) >= (nrxl - 1) ) .AND. ( pos(1) < nrxh )
+                okp(2) = ( pos(2) >= (nryl - 1) ) .AND. ( pos(2) < nryh ) 
+                okp(3) = ( pos(3) >= (nrzl - 1) ) .AND. ( pos(3) < nrzh ) 
+                IF( okp(1) .AND. okp(2) .AND. okp(3) ) THEN
+                  natwin( nsp ) = natwin( nsp ) + 1
+                END IF
               END DO
             END DO
           END DO
         END DO
       END DO
 
-      x_0ang = -DBLE(ngx+1) / DBLE(ngx) * dirc(1,1) - &
-                DBLE(ngy+1) / DBLE(ngy) * dirc(2,1) - &
-                DBLE(ngz+1) / DBLE(ngz) * dirc(3,1)
-      y_0ang = -DBLE(ngx+1) / DBLE(ngx) * dirc(1,2) - &
-                DBLE(ngy+1) / DBLE(ngy) * dirc(2,2) - &
-                DBLE(ngz+1) / DBLE(ngz) * dirc(3,2)
-      z_0ang = -DBLE(ngx+1) / DBLE(ngx) * dirc(1,3) - &
-                DBLE(ngy+1) / DBLE(ngy) * dirc(2,3) - &
-                DBLE(ngz+1) / DBLE(ngz) * dirc(3,3)
+      ALLOCATE( poscarwin( 3, MAXVAL( natwin ), nspec ) )
 
-      x_0ang = x_0ang / bohr
-      y_0ang = y_0ang / bohr
-      z_0ang = z_0ang / bohr
+      natwin = 0
+      DO nsp = 1, nspec
+        DO ni = 1, nionsp(nsp)
+          DO nx = -2, 2
+            DO ny = -2, 2
+              DO nz = -2, 2
+                pos(1) = ( posion(1,ni,nsp) + DBLE(nx) ) * ngx
+                pos(2) = ( posion(2,ni,nsp) + DBLE(ny) ) * ngy
+                pos(3) = ( posion(3,ni,nsp) + DBLE(nz) ) * ngz
+                okp(1) = ( pos(1) >= (nrxl - 1) ) .AND. ( pos(1) < nrxh )
+                okp(2) = ( pos(2) >= (nryl - 1) ) .AND. ( pos(2) < nryh ) 
+                okp(3) = ( pos(3) >= (nrzl - 1) ) .AND. ( pos(3) < nrzh ) 
+                IF( okp(1) .AND. okp(2) .AND. okp(3) ) THEN
+                  natwin( nsp ) = natwin( nsp ) + 1
+                  DO m = 1, 3
+                    poscarwin( m, natwin( nsp ), nsp ) = &
+                      ( pos(1) * dirc(1,m) / ngx + pos(2) * dirc(2,m) / ngy + pos(3) * dirc(3,m) / ngz ) / bohr
+                  END DO
+                END IF
+              END DO
+            END DO
+          END DO
+        END DO
+      END DO
 
       DO i = 1, 3
         dirl(1,i) = dirc(1,i) / ngx / bohr
@@ -298,19 +290,22 @@
         dirl(3,i) = dirc(3,i) / ngz / bohr
       END DO
 
-      nb = nwann
+      aa = 0.0
+      x_0ang = ( nrxl - aa ) * dirl( 1, 1 ) + ( nryl - aa ) * dirl( 2, 1 ) + ( nrzl - aa ) * dirl( 3, 1 )
+      y_0ang = ( nrxl - aa ) * dirl( 1, 2 ) + ( nryl - aa ) * dirl( 2, 2 ) + ( nrzl - aa ) * dirl( 3, 2 )
+      z_0ang = ( nrxl - aa ) * dirl( 1, 3 ) + ( nryl - aa ) * dirl( 2, 3 ) + ( nrzl - aa ) * dirl( 3, 3 )
 
-      IF ( nb <= 9 ) THEN
-        WRITE( frfft, ' ( ''WFR00'', i1, ''.gau'' ) ' ) nb
-        WRITE( fifft, ' ( ''WFI00'', i1, ''.gau'' ) ' ) nb
-      ELSE IF ( nb <= 99 ) THEN
-        WRITE( frfft, ' ( ''WFR0'', i2, ''.gau'' ) ' ) nb
-        WRITE( fifft, ' ( ''WFI0'', i2, ''.gau'' ) ' ) nb
-      ELSE IF ( nb <= 999 ) THEN
-        WRITE( frfft, ' ( ''WFR'', i3, ''.gau'' ) ' ) nb
-        WRITE( fifft, ' ( ''WFI'', i3, ''.gau'' ) ' ) nb
+      IF ( nwann <= 9 ) THEN
+        WRITE( frfft, ' ( ''WFR00'', i1, ''.gau'' ) ' ) nwann
+        WRITE( fifft, ' ( ''WFI00'', i1, ''.gau'' ) ' ) nwann
+      ELSE IF ( nwann <= 99 ) THEN
+        WRITE( frfft, ' ( ''WFR0'', i2, ''.gau'' ) ' ) nwann
+        WRITE( fifft, ' ( ''WFI0'', i2, ''.gau'' ) ' ) nwann
+      ELSE IF ( nwann <= 999 ) THEN
+        WRITE( frfft, ' ( ''WFR'', i3, ''.gau'' ) ' ) nwann
+        WRITE( fifft, ' ( ''WFI'', i3, ''.gau'' ) ' ) nwann
       ELSE
-        WRITE(*,*) ' ERROR - nb .gt. 999'
+        WRITE(*,*) ' ERROR - nwann .gt. 999'
       END IF
 
 
@@ -318,45 +313,71 @@
 
       nionst = SUM( nionsp( 1:nspec ) )
 
-      WRITE(39, '(i4,3f12.6)' ) nionst*ngt**3, x_0ang, y_0ang, z_0ang
-      WRITE(39, '(i4,3f12.6)' ) ngt*ngx, ( dirl(1,i), i=1,3 )
-      WRITE(39, '(i4,3f12.6)' ) ngt*ngy, ( dirl(2,i), i=1,3 )
-      WRITE(39, '(i4,3f12.6)' ) ngt*ngz, ( dirl(3,i), i=1,3 )
+      WRITE(39, '(i4,3f12.6)' ) SUM( natwin ), x_0ang, y_0ang, z_0ang
+      WRITE(39, '(i4,3f12.6)' ) (nrxh-nrxl+1), ( dirl(1,i), i=1,3 )
+      WRITE(39, '(i4,3f12.6)' ) (nryh-nryl+1), ( dirl(2,i), i=1,3 )
+      WRITE(39, '(i4,3f12.6)' ) (nrzh-nrzl+1), ( dirl(3,i), i=1,3 )
 
+      CALL convert_label( nameat, indat, nspec )
       DO nsp = 1 , nspec
-        DO ni = 1 , nionsp(nsp) * ngt**3
-          WRITE(39, '(i4,4e13.5)' ) 5 + nsp, 1.d0, ( poscartbig(i,ni,nsp), i=1,3 )
+        DO ni = 1 , natwin( nsp )
+          WRITE(39, '(i4,4e13.5)' ) indat( nsp ), 1.d0, ( poscarwin( i, ni, nsp ), i=1,3 )
         END DO
       END DO
 
-      nmod = MOD( ngt*ngz, 6 )
-      nt = ngt * ngz
-      nt = nt / 6
-      nlim = -ngz + (nt-1) * 6
+      nmod = MOD( ( nrzh - nrzl + 1 ), 6 )
+      nt   = ( nrzh - nrzl + 1 ) / 6
+      nlim = nrzl + ( nt * 6 ) - 1
 
-      DO nx = -ngx, ngs*ngx-1
-        DO ny = -ngy,ngs*ngy-1
-          DO nz = -ngz, nlim, 6
-            WRITE( 39,'(6e13.5)' ) ( REAL( cwann(nx,ny,nzz) ), nzz = nz, nz+5 )
+      DO nx = nrxl, nrxh
+        DO ny = nryl, nryh
+          DO nz = nrzl, nlim , 6
+            WRITE( 39, fmt = '(6e13.5)' ) ( REAL( cwann( nx, ny, nzz ) ), nzz = nz, nz + 5 )
           END DO
-          IF ( nmod == 1 ) THEN
-            WRITE(39,' (e13.5)' )  REAL( cwann(nx,ny,nlim+6) )
-          ELSE IF ( nmod == 2 ) THEN
-            WRITE(39,' (2e13.5)' ) ( REAL( cwann(nx,ny,nzz) ), nzz = nlim+6, nlim+7 )
-          ELSE IF ( nmod == 3 ) THEN
-            WRITE(39,' (3e13.5)' ) ( REAL( cwann(nx,ny,nzz) ), nzz = nlim+6, nlim+8 )
-          ELSE IF ( nmod == 4 ) THEN
-            WRITE(39,' (4e13.5)' ) ( REAL( cwann(nx,ny,nzz) ), nzz = nlim+6, nlim+9 )
-          ELSE IF ( nmod == 5 ) THEN
-            WRITE(39,' (5e13.5)' ) ( REAL( cwann(nx,ny,nzz) ), nzz = nlim+6, nlim+10 )
-          END IF
+          WRITE( 39, fmt = '(5e13.5)' ) ( REAL( cwann( nx, ny, nzz) ), nzz = nlim + 1, nlim + nmod )
         END DO
       END DO
 
       CLOSE(39)
 
-      ! write( *, * ) nb, nwann, selected_int_kind( nwann )
+      DEALLOCATE( cwann )
+      DEALLOCATE( poscarwin )
+
       call gcube2plt( nwann )
 
       STOP '*** THE END *** (plot.f90)'
       END
+
+
+      SUBROUTINE convert_label( nameat, indat, ntyp )
+        USE parameters, ONLY: npsx, natx
+        CHARACTER( LEN=2 ) :: nameat( npsx )
+        INTEGER :: indat( npsx )
+        INTEGER :: ntyp
+
+        DO is = 1, ntyp
+          SELECT CASE ( TRIM( nameat( is ) ) )
+            CASE ( 'H' )
+              indat( is ) = 1
+            CASE ( 'He' )
+              indat( is ) = 2
+            CASE ( 'Li' )
+              indat( is ) = 3
+            CASE DEFAULT
+              indat( is ) = 6
+          END SELECT
+        END DO
+
+!"H",                                                                                "He",
+!"Li","Be",                                                  "B","C","N","O","F","Ne",
+!"Na","Mg",                                                  "Al","Si","P","S","Cl","Ar",
+!"K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr",
+!"Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","St","Sb","Te","I","Xe",
+!"Cs","Ba","La","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn",
+!"Fr","Ra","Ac",
+!               "Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",
+!               "Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr"
+
+
+        RETURN
+      END SUBROUTINE
