a1916da2ca739a054729f4885cf493689ecf926a
arrigo <arrigo@0e440500-e291-4b27-a0db-898e6d94e59d>
2004-07-28 16:32:52 +0000

    aggiunti gli input del test 2.... l'unica cosa che manca e' il conto finale per la conduttanza del sistema composto 2 - 4 - 2 (Au atoms) o anche e forse meglio 4 - 2 -4 ANDREA
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk/want@103 0e440500-e291-4b27-a0db-898e6d94e59d

--- tests/test06/run.sh
+++ tests/test06/run.sh
@@ -18,21 +18,35 @@ MANUAL=" Usage
  where FLAG is one of the following 
  (no FLAG will print this manual page) :
  
- scf             PWSCF self-consistent calculation
- nscf            PWSCF non-self-consistent calculation
- pw2wan          export PWSCF data to WanT package
- pwscf           perform SCF, NSCF, PW2WAN all together
- window          determine which bads are in the energy window
-                 of interest and then write data accoding to it
- disentangle     select the optimal subspace on which perform
-                 the wannier minimization
- wannier         perform the above cited minimization
- hamiltonian     writes the hamiltonian matrix elements on the Wannier basis
- want            perform WINDOW, DISENTANGLE, WANNIER and HAMILTONIAN all together 
- bulk            evaluate the transmittance, for the bulk case
- all             perform all the above described steps
+ scf_cond             PWSCF self-consistent calculation (conductor)
+ nscf_cond            PWSCF non-self-consistent calculation (conductor)
+ pw2wan_cond          export PWSCF data (conductor) to WanT package
+ pwscf_cond           perform SCF, NSCF, PW2WAN all together (conductor)
+ window_cond          wannier energy window (conductor)
+ disentangle_cond     wannier subspace definition (conductor)
+ wannier_cond         wannier functions (conductor)
+ hamiltonian_cond     writes the hamiltonian on the Wannier basis (conductor)
+ want_cond            all the wannier function steps, WINDOW, DISENT., WANNIER (conductor)
+ all_cond             PWSCF_COND and WANT_COND tigether
 
- clean           delete all output files and the temporary directory
+ scf_leads            PWSCF self-consistent calculation (leads)
+ nscf_leads           PWSCF non-self-consistent calculation (leads)
+ pw2wan_leads         export PWSCF data (leads) to WanT package
+ pwscf_leads          perform SCF, NSCF, PW2WAN all together (leads)
+ window_leads         wannier energy window (leads)
+ disentangle_leads    wannier subspace definition (leads)
+ wannier_leads        wannier functions (leads)
+ hamiltonian_leads    writes the hamiltonian on the Wannier basis (leads)
+ want_leads           all the wannier function steps, WINDOW, DISENT., WANNIER (leads)
+ all_leads            PWSCF_LEADS and WANT_LEADS tigether
+
+ pwscf                PWSCF_COND and PWSCF_LEADS toether
+ want                 WANT_COND and WANT_LEADS together
+ conductor            evaluate the transmittance for the general conductor geometry
+ bulk                 evaluate the transmittance for the conductor region treated as a bulk
+ all                  perform all the above described steps
+
+ clean                delete all output files and the temporary directory
 "
 #
 #================================================================
@@ -49,153 +63,332 @@ TEST_NAME=Test2
 #
 # evaluate the starting choice about what is to run 
 
-SCF=
-NSCF=
-PW2WAN=
-WINDOW=
-DISENTANGLE=
-WANNIER=
-HAMILTONIAN=
+SCF_COND=
+NSCF_COND=
+PW2WAN_COND=
+WINDOW_COND=
+DISENTANGLE_COND=
+WANNIER_COND=
+HAMILTONIAN_COND=
+
+SCF_LEADS=
+NSCF_LEADS=
+PW2WAN_LEADS=
+WINDOW_LEADS=
+DISENTANGLE_LEADS=
+WANNIER_LEADS=
+HAMILTONIAN_LEADS=
+
+CONDUCTOR=
 BULK=
 CLEAN=
+
+
+
 
 if [ $# = 0 ] ; then echo "$MANUAL" ; exit 0 ; fi
 INPUT=`echo $1 | tr [:upper:] [:lower:]`
 
 case $INPUT in 
-   (scf)            SCF=".TRUE." ;;
-   (nscf)           NSCF=".TRUE." ;;
-   (pw2wan)         PW2WAN=".TRUE." ;;
-   (pwscf)          SCF=".TRUE." ; NSCF=".TRUE." ; PW2WAN=".TRUE." ;;
-   (window)         WINDOW=".TRUE." ;;
-   (disentangle)    DISENTANGLE=".TRUE." ;;
-   (wannier)        WANNIER=".TRUE." ;;
-   (hamiltonian)    HAMILTONIAN=".TRUE." ;;
-   (want)           WINDOW=".TRUE." ; DISENTANGLE=".TRUE." ; WANNIER=".TRUE." ;
-                    HAMILTONIAN=".TRUE." ;;
-   (bulk)           BULK=".TRUE." ;;
-   (all)            SCF=".TRUE." ; NSCF=".TRUE." ; PW2WAN=".TRUE." ; 
-                    WINDOW=".TRUE." ; DISENTANGLE=".TRUE." ; WANNIER=".TRUE." ; 
-                    BULK=".TRUE." ;;
-   (clean)          CLEAN=".TRUE." ;;
+   (scf_cond)            SCF_COND=".TRUE." ;;
+   (nscf_cond)           NSCF_COND=".TRUE." ;;
+   (pw2wan_cond)         PW2WAN_COND=".TRUE." ;;
+   (pwscf_cond)          SCF_COND=".TRUE." ; NSCF_COND=".TRUE." ; PW2WAN_COND=".TRUE." ;;
+   (window_cond)         WINDOW_COND=".TRUE." ;;
+   (disentangle_cond)    DISENTANGLE_COND=".TRUE." ;;
+   (wannier_cond)        WANNIER_COND=".TRUE." ;;
+   (hamiltonian_cond)    HAMILTONIAN_COND=".TRUE." ;;
+   (want_cond)           WINDOW_COND=".TRUE." ; DISENTANGLE_COND=".TRUE." ; 
+                         WANNIER_COND=".TRUE." ; HAMILTONIAN_COND=".TRUE." ;;
+   (all_cond)            SCF_COND=".TRUE." ; NSCF_COND=".TRUE." ; PW2WAN_COND=".TRUE." ;
+                         WINDOW_COND=".TRUE." ; DISENTANGLE_COND=".TRUE." ;
+                         WANNIER_COND=".TRUE." ; HAMILTONIAN_COND=".TRUE." ;;
+
+   (scf_leads)           SCF_LEADS=".TRUE." ;;
+   (nscf_leads)          NSCF_LEADS=".TRUE." ;;
+   (pw2wan_leads)        PW2WAN_LEADS=".TRUE." ;;
+   (pwscf_leads)         SCF_LEADS=".TRUE." ; NSCF_LEADS=".TRUE." ; PW2WAN_LEADS=".TRUE." ;;
+   (window_leads)        WINDOW_LEADS=".TRUE." ;;
+   (disentangle_leads)   DISENTANGLE_LEADS=".TRUE." ;;
+   (wannier_leads)       WANNIER_LEADS=".TRUE." ;;
+   (hamiltonian_leads)   HAMILTONIAN_LEADS=".TRUE." ;;
+   (want_leads)          WINDOW_LEADS=".TRUE." ; DISENTANGLE_LEADS=".TRUE." ; 
+                         WANNIER_LEADS=".TRUE." ; HAMILTONIAN_LEADS=".TRUE." ;;
+   (all_leads)           SCF_LEADS=".TRUE." ; NSCF_LEADS=".TRUE." ; PW2WAN_LEADS=".TRUE." ;
+                         WINDOW_LEADS=".TRUE." ; DISENTANGLE_LEADS=".TRUE." ;
+                         WANNIER_LEADS=".TRUE." ; HAMILTONIAN_LEADS=".TRUE." ;;
+
+   (pwscf)               SCF_COND=".TRUE." ; NSCF_COND=".TRUE." ; PW2WAN_COND=".TRUE." ;
+                         SCF_LEADS=".TRUE." ; NSCF_LEADS=".TRUE." ; PW2WAN_LEADS=".TRUE." ;;
+   (want)                WINDOW_COND=".TRUE." ; DISENTANGLE_COND=".TRUE." ;
+                         WANNIER_COND=".TRUE." ; HAMILTONIAN_COND=".TRUE." ;
+                         WINDOW_LEADS=".TRUE." ; DISENTANGLE_LEADS=".TRUE." ;
+                         WANNIER_LEADS=".TRUE." ; HAMILTONIAN_LEADS=".TRUE." ;;
+   (conductor)           CONDUCTOR=".TRUE." ;;
+   (bulk)                BULK=".TRUE." ;;
+   (all)                 SCF_COND=".TRUE." ; NSCF_COND=".TRUE." ; PW2WAN_COND=".TRUE." ; 
+                         WINDOW_COND=".TRUE." ; DISENTANGLE_COND=".TRUE." ; 
+                         WANNIER_COND=".TRUE." ;  HAMILTONIAN_COND=".TRUE." ;
+                         SCF_LEADS=".TRUE." ; NSCF_LEADS=".TRUE." ; PW2WAN_LEADS=".TRUE." ; 
+                         WINDOW_LEADS=".TRUE." ; DISENTANGLE_LEADS=".TRUE." ; 
+                         WANNIER_LEADS=".TRUE." ;  HAMILTONIAN_LEADS=".TRUE." ;
+                         CONDUCTOR=".TRUE." ;;
+
+   (clean)               CLEAN=".TRUE." ;;
+   (*)                   echo " Invalid input FLAG, type ./run.sh for help" ; exit 1 ;;  
 esac
+
 
 #
 # preliminaries
 #
-cd $TMPDIR
-test -e $TEST_NAME || mkdir $TEST_NAME 
-cd $TEST_NAME
-ln -sf $TEST_HOME/../Pseudo/* .
-ln -sf $TMPDIR/$TEST_NAME $TEST_HOME/SCRATCH
-ln -sf $TEST_HOME $TMPDIR/$TEST_NAME/HOME
+test -e $TMPDIR/$TEST_NAME || mkdir $TMPDIR/$TEST_NAME 
+cd $TMPDIR/$TEST_NAME
+ln -sf $TEST_HOME/../Pseudo/*.UPF .
+if [ ! -e $TEST_HOME/SCRATCH ] ; then
+    cd $TEST_HOME
+    ln -sf $TMPDIR/$TEST_NAME ./SCRATCH
+fi
+if [ ! -e $TMPDIR/$TEST_NAME/HOME ] ; then
+    cd $TMPDIR/$TEST_NAME
+    ln -sf $TEST_HOME ./HOME
+fi
 
-##
-## running PWSCF SCF
-##
-#if [ "$SCF" = ".TRUE." ] ; then  
-#   $PARA_PREFIX  $PWSCF_BIN/pw.x $PARA_POSTFIX < $TEST_HOME/scf.in > $TEST_HOME/scf.out
-#   if [ $? = 0 ] ; then 
-#      echo "SCF calculation done" 
-#   else
-#      echo "found some problems in SCF calculation, stopping" ; exit 1
-#   fi
-#fi
 #
-##
-## running PWSCF NSCF
-##
-#if [ "$NSCF" = ".TRUE." ] ; then  
-#   $PARA_PREFIX  $PWSCF_BIN/pw.x $PARA_POSTFIX < $TEST_HOME/nscf.in > $TEST_HOME/nscf.out
-#   if [ $? = 0 ] ; then 
-#      echo "NSCF calculation done" 
-#   else
-#      echo "found some problems in NSCF calculation, stopping" ; exit 1
-#   fi
-#fi
-#   
-##
-## running PWSCF PW2WAN
-##
-#if [ "$PW2WAN" = ".TRUE." ] ; then  
-#   $PARA_PREFIX  $PWSCF_BIN/pw2wan.x $PARA_POSTFIX  \
-#              <  $TEST_HOME/pw2wan.in > $TEST_HOME/pw2wan.out
-#   if [ $? = 0 ] ; then 
-#      echo "PW2WAN calculation done" 
-#   else
-#      echo "found some problems in PW2WAN calculation, stopping" ; exit 1
-#   fi
-#fi
+# 2 sub scratch directories
+test -e $TMPDIR/$TEST_NAME/COND || mkdir $TMPDIR/$TEST_NAME/COND
+test -e $TMPDIR/$TEST_NAME/LEADS || mkdir $TMPDIR/$TEST_NAME/LEADS
+
+
+#-----------------------------------------------------------------------------
+cd $TMPDIR/$TEST_NAME
+
 #
-##
-## running WINDOW
-##
-#if [ "$WINDOW" = ".TRUE." ] ; then  
-#   $WANT_BIN/window.x < $TEST_HOME/want.in > $TEST_HOME/window.out
-#   if [ $? = 0 ] ; then 
-#      echo "WINDOW calculation done" 
-#   else
-#      echo "found some problems in WINDOW calculation, stopping" ; exit 1
-#   fi
-#fi
+# running PWSCF SCF
 #
-##
-## running DISENTANGLE
-##
-#if [ "$DISENTANGLE" = ".TRUE." ] ; then  
-#   $WANT_BIN/disentangle.x < $TEST_HOME/want.in > $TEST_HOME/disentangle.out
-#   if [ $? = 0 ] ; then 
-#      echo "DISENTANGLE calculation done" 
-#   else
-#      echo "found some problems in DISENTANGLE calculation, stopping" ; exit 1
-#   fi
-#fi
+if [ "$SCF_COND" = ".TRUE." ] ; then  
+   cd COND
+   $PARA_PREFIX  $PWSCF_BIN/pw.x $PARA_POSTFIX \ 
+                       < $TEST_HOME/scf_cond.in > $TEST_HOME/scf_cond.out
+   if [ $? = 0 ] ; then 
+      echo "SCF_COND calculation done" 
+   else
+      echo "found some problems in SCF_COND calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
 #
-##
-## running WANNIER
-##
-#if [ "$WANNIER" = ".TRUE." ] ; then  
-#   $WANT_BIN/wannier.x < $TEST_HOME/want.in > $TEST_HOME/wannier.out
-#   if [ $? = 0 ] ; then 
-#      echo "WANNIER calculation done" 
-#   else
-#      echo "found some problems in WANNIER calculation, stopping" ; exit 1
-#   fi
-#fi
+if [ "$SCF_LEADS" = ".TRUE." ] ; then  
+   cd LEADS
+   $PARA_PREFIX  $PWSCF_BIN/pw.x $PARA_POSTFIX \ 
+                       < $TEST_HOME/scf_leads.in > $TEST_HOME/scf_leads.out
+   if [ $? = 0 ] ; then 
+      echo "SCF_LEADS calculation done" 
+   else
+      echo "found some problems in SCF_LEADS calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+
+
 #
-##
-## running HAMILTONIAN
-##
-#if [ "$HAMILTONIAN" = ".TRUE." ] ; then  
-#   $WANT_BIN/hamiltonian.x < $TEST_HOME/hamiltonian.in > $TEST_HOME/hamiltonian.out
-#   if [ $? = 0 ] ; then 
-#      echo "HAMILTONIAN calculation done" 
-#      #
-#      # hopefully will be improoved very soon...
-#      #
-#      ln -sf fort.103 H00.dat
-#      ln -sf fort.104 H01.dat
-#   else
-#      echo "found some problems in HAMILTONIAN calculation, stopping" ; exit 1
-#   fi
-#fi
+# running PWSCF NSCF
 #
+if [ "$NSCF_COND" = ".TRUE." ] ; then  
+   cd COND
+   $PARA_PREFIX  $PWSCF_BIN/pw.x $PARA_POSTFIX  < $TEST_HOME/nscf_cond.in > $TEST_HOME/nscf_cond.out
+   if [ $? = 0 ] ; then 
+      echo "NSCF_COND calculation done" 
+   else
+      echo "found some problems in NSCF_COND calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
 #
-##
-## running BULK
-##
-#if [ "$BULK" = ".TRUE." ] ; then  
-#   $TRANS_BIN/bulk.x < $TEST_HOME/bulk.in > $TEST_HOME/bulk.out
-#   if [ $? = 0 ] ; then 
-#      echo "BULK calculation done" 
-#      #
-#      # also this needs to be improoved
-#      #
-#      cp dos.out cond.out $TEST_HOME
-#   else
-#      echo "found some problems in BULK calculation, stopping" ; exit 1
-#   fi
-#fi
+if [ "$NSCF_LEADS" = ".TRUE." ] ; then  
+   cd LEADS
+   $PARA_PREFIX  $PWSCF_BIN/pw.x $PARA_POSTFIX \
+                     < $TEST_HOME/nscf_leads.in > $TEST_HOME/nscf_leads.out
+   if [ $? = 0 ] ; then 
+      echo "NSCF_LEADS calculation done" 
+   else
+      echo "found some problems in NSCF_LEADS calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+   
+#
+# running PWSCF PW2WAN
+#
+if [ "$PW2WAN_COND" = ".TRUE." ] ; then  
+   cd COND
+   $PARA_PREFIX  $PWSCF_BIN/pw2wan.x $PARA_POSTFIX \
+              <  $TEST_HOME/pw2wan_cond.in > $TEST_HOME/pw2wan_cond.out
+   if [ $? = 0 ] ; then 
+      echo "PW2WAN_COND calculation done" 
+   else
+      echo "found some problems in PW2WAN_COND calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+#
+if [ "$PW2WAN_LEADS" = ".TRUE." ] ; then  
+   cd LEADS
+   $PARA_PREFIX  $PWSCF_BIN/pw2wan.x $PARA_POSTFIX  \
+              <  $TEST_HOME/pw2wan_leads.in > $TEST_HOME/pw2wan_leads.out
+   if [ $? = 0 ] ; then 
+      echo "PW2WAN_LEADS calculation done" 
+   else
+      echo "found some problems in PW2WAN_LEADS calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+
+#
+# running WINDOW
+#
+if [ "$WINDOW_COND" = ".TRUE." ] ; then  
+   cd COND
+   $WANT_BIN/window.x < $TEST_HOME/want_cond.in > $TEST_HOME/window_cond.out
+   if [ $? = 0 ] ; then 
+      echo "WINDOW_COND calculation done" 
+   else
+      echo "found some problems in WINDOW_COND calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+if [ "$WINDOW_LEADS" = ".TRUE." ] ; then  
+   cd LEADS
+   $WANT_BIN/window.x < $TEST_HOME/want_leads.in > $TEST_HOME/window_leads.out
+   if [ $? = 0 ] ; then 
+      echo "WINDOW_LEADS calculation done" 
+   else
+      echo "found some problems in WINDOW_LEADS calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+
+#
+# running DISENTANGLE
+#
+if [ "$DISENTANGLE_COND" = ".TRUE." ] ; then  
+   cd COND
+   $WANT_BIN/disentangle.x < $TEST_HOME/want_cond.in > $TEST_HOME/disentangle_cond.out
+   if [ $? = 0 ] ; then 
+      echo "DISENTANGLE_COND calculation done" 
+   else
+      echo "found some problems in DISENTANGLE_COND calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+if [ "$DISENTANGLE_LEADS" = ".TRUE." ] ; then  
+   cd LEADS
+   $WANT_BIN/disentangle.x < $TEST_HOME/want_leads.in > $TEST_HOME/disentangle_leads.out
+   if [ $? = 0 ] ; then 
+      echo "DISENTANGLE_LEADS calculation done" 
+   else
+      echo "found some problems in DISENTANGLE_LEADS calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+
+#
+# running WANNIER
+#
+if [ "$WANNIER_COND" = ".TRUE." ] ; then  
+   cd COND
+   $WANT_BIN/wannier.x < $TEST_HOME/want_cond.in > $TEST_HOME/wannier_cond.out
+   if [ $? = 0 ] ; then 
+      echo "WANNIER_COND calculation done" 
+   else
+      echo "found some problems in WANNIER_COND calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+if [ "$WANNIER_LEADS" = ".TRUE." ] ; then  
+   cd LEADS
+   $WANT_BIN/wannier.x < $TEST_HOME/want_leads.in > $TEST_HOME/wannier_leads.out
+   if [ $? = 0 ] ; then 
+      echo "WANNIER_LEADS calculation done" 
+   else
+      echo "found some problems in WANNIER_LEADS calculation, stopping" ; exit 1
+   fi
+   cd ..
+fi
+
+#
+# running HAMILTONIAN
+#
+if [ "$HAMILTONIAN_COND" = ".TRUE." ] ; then  
+   cd COND
+   $WANT_BIN/hamiltonian.x < $TEST_HOME/hamiltonian_cond.in  \
+                           > $TEST_HOME/hamiltonian_cond.out
+   if [ $? = 0 ] ; then 
+      echo "HAMILTONIAN_COND calculation done" 
+   else
+      echo "found some problems in HAMILTONIAN_COND calculation, stopping" ; exit 1
+   fi
+   cd ..
+   ln -sf COND/fort.102 H00_C.dat
+   ln -sf COND/fort.103 H01_CB.dat
+   ln -sf COND/fort.103 H01_AC.dat
+fi
+if [ "$HAMILTONIAN_LEADS" = ".TRUE." ] ; then  
+   cd LEADS
+   $WANT_BIN/hamiltonian.x < $TEST_HOME/hamiltonian_leads.in  \
+                           > $TEST_HOME/hamiltonian_leads.out
+   if [ $? = 0 ] ; then 
+      echo "HAMILTONIAN_LEADS calculation done" 
+   else
+      echo "found some problems in HAMILTONIAN_LEADS calculation, stopping" ; exit 1
+   fi
+   cd ..
+   ln -sf LEADS/fort.103 H00_A.dat
+   ln -sf LEADS/fort.103 H00_B.dat
+fi
+
+
+#
+# running CONDUCTOR
+#
+if [ "$CONDUCTOR" = ".TRUE." ] ; then  
+   $TRANS_BIN/conductor.x < $TEST_HOME/conductor.in > $TEST_HOME/conductor.out
+   if [ $? = 0 ] ; then 
+      echo "CODNDUCTOR calculation done" 
+      #
+      # also this needs to be improoved
+      #
+      cp dos.out cond.out $TEST_HOME
+   else
+      echo "found some problems in CONDUCTOR calculation, stopping" ; exit 1
+   fi
+fi
+
+
+#
+# running BULK  (eventually)
+#
+if [ "$BULK" = ".TRUE." ] ; then  
+   ln -sf COND/fort.103 H00.dat
+   ln -sf COND/fort.104 H01.dat
+   #ln -sf LEADS/fort.102 H00.dat
+   #ln -sf LEADS/fort.103 H01.dat
+   #ln -sf LEADS/fort.103 H00.dat
+   #ln -sf LEADS/fort.104 H01.dat
+   #ln -sf LEADS/fort.105 H00.dat
+   #ln -sf LEADS/fort.106 H01.dat
+   $TRANS_BIN/bulk.x < $TEST_HOME/bulk.in > $TEST_HOME/bulk.out
+   if [ $? = 0 ] ; then 
+      echo "BULK calculation done" 
+      #
+      # also this needs to be improoved
+      #
+      mv dos.out  $TEST_HOME/dos_bulk.out
+      mv cond.out $TEST_HOME/cond_bulk.out
+   else
+      echo "found some problems in BULK calculation, stopping" ; exit 1
+   fi
+fi
 
 
 
@@ -204,11 +397,13 @@ ln -sf $TEST_HOME $TMPDIR/$TEST_NAME/HOME
 #
 if [ "$CLEAN" = ".TRUE." ] ; then  
    cd $TEST_HOME
-      test -e *.out && rm *.out
+      rm -rf *.out
       test -e SCRATCH && rm SCRATCH
    cd $TMPDIR
       test -e $TEST_NAME/HOME && rm $TEST_NAME/HOME
       test -d $TEST_NAME && rm -rf $TEST_NAME
+      test -d $TEST_NAME/COND && rm -rf $TEST_NAME/COND
+      test -d $TEST_NAME/LEADS && rm -rf $TEST_NAME/LEADS
       echo "CLEAN done" 
 fi
 
@@ -216,11 +411,6 @@ fi
 # exiting
 echo "run.sh : everything done"
 exit 0
-
-
-
-
-
 
 
 
