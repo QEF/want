30a9a67bba72bac9b43e86d9685e367ecc55f959
arrigo <arrigo@0e440500-e291-4b27-a0db-898e6d94e59d>
2004-11-16 16:09:52 +0000

    me ne ero dimenticato quando ho aggiunto vari nuovi file... ANDREA
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk/want@200 0e440500-e291-4b27-a0db-898e6d94e59d

---
+++ wannier/io_module.f90
@@ -0,0 +1,238 @@
+!
+! Copyright (C) 2004 Andrea Ferretti
+!
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+! <INFO>
+!*********************************************
+   MODULE io_module
+!*********************************************
+   USE parameters, ONLY : nstrx
+   USE iotk_module
+   IMPLICIT NONE
+   PRIVATE
+   SAVE
+
+!
+! Contains basic data concerning IO
+! 
+! contains:
+! SUBROUTINE  ioname(data,filename[,lpostfix][,lpath])
+! SUBROUTINE  read_iodata(unit,name,prefix,postfix,work_dir,title,found)
+! SUBROUTINE  write_iodata(unit,name,prefix,postfix,work_dir,title)
+!
+! DATA in ioname routine should be:
+! 
+! * 'dft_data'
+! * 'subspace'
+! * 'wannier'
+! * 'self_energy'
+! * 'hamiltonian'
+! * 'save'
+!
+! </INFO>
+!
+   INTEGER, PARAMETER         ::   &
+         stdout = 6,               &! std output unit
+       dft_unit = 10,              &! input file (DFT data) unit
+     space_unit = 11,              &! dos and pdos unit
+       wan_unit = 12,              &! G integrals unit
+       sgm_unit = 13,              &! SGM unit
+       ham_unit = 14,              &! HAM unit
+      save_unit = 99                ! restart file unit
+
+
+   CHARACTER(7), PARAMETER    ::  suffix_dft_data=".XMLpun"
+   CHARACTER(7), PARAMETER    ::  suffix_subspace=".spaces"
+   CHARACTER(4), PARAMETER    ::  suffix_wannier=".wan"
+   CHARACTER(4), PARAMETER    ::  suffix_self_energy=".sgm"
+   CHARACTER(4), PARAMETER    ::  suffix_hamiltonian=".ham"
+   CHARACTER(5), PARAMETER    ::  suffix_save=".save"
+   
+   CHARACTER(nstrx)           :: prefix
+   CHARACTER(nstrx)           :: postfix
+   CHARACTER(nstrx)           :: work_dir
+   CHARACTER(nstrx)           :: title
+         
+   INTEGER :: ionode_id = 0
+   LOGICAL :: ionode = .TRUE.
+   LOGICAL :: first = .TRUE.
+
+!
+! end delcarations
+!
+
+
+   PUBLIC ::  io_global_start, io_global_getionode
+   PUBLIC ::  ionode, ionode_id
+
+   PUBLIC ::  stdout, dft_unit, space_unit, wan_unit, &
+              sgm_unit, ham_unit, save_unit
+   PUBLIC ::  prefix, postfix, work_dir, title
+   PUBLIC ::  ioname
+   PUBLIC ::  read_iodata
+   PUBLIC ::  write_iodata
+
+
+   CONTAINS
+
+!
+! subroutines
+!
+
+!**********************************************************
+   SUBROUTINE ioname(data,filename,lpostfix,lpath)
+   !**********************************************************
+      IMPLICIT NONE
+      CHARACTER(*),       INTENT(in)  :: data
+      CHARACTER(*),       INTENT(out) :: filename
+      LOGICAL, OPTIONAL,  INTENT(in)  :: lpostfix, lpath   
+      LOGICAL             :: lpostfix_, lpath_
+      CHARACTER(nstrx)    :: path_, postfix_, suffix_
+      INTEGER             :: length
+
+      !
+      ! DEFAULT
+      lpostfix_ = .TRUE.
+      lpath_ = .TRUE.
+      IF ( PRESENT(lpostfix) ) lpostfix_ = lpostfix
+      IF ( PRESENT(lpath) )     lpath_ = lpath
+      
+      !
+      ! setting the base name
+      path_ = " " 
+      postfix_ = " " 
+      IF ( lpath_ )  path_ = TRIM(work_dir)
+      IF ( lpostfix_ ) postfix_ = TRIM(postfix)
+
+      !
+      ! add the / if needed
+      length = LEN_TRIM( path_ )
+      IF ( length /= 0 .AND. path_(length:length) /= "/"  ) &
+           path_ = TRIM(path_)//"/"
+          
+
+      SELECT CASE( TRIM(data) )
+      CASE ( "dft_data" ) 
+           suffix_ = TRIM(suffix_dft_data)
+      CASE ( "subspace" ) 
+           suffix_ = TRIM(suffix_subspace)
+      CASE ( "wannier" ) 
+           suffix_ = TRIM(suffix_wannier)
+      CASE ( "self_energy" )
+           suffix_ = TRIM(suffix_self_energy)
+      CASE ( "hamiltonian" )
+           suffix_ = TRIM(suffix_hamiltonian)
+      CASE ( "save" )
+           suffix_ = TRIM(suffix_save)
+      CASE DEFAULT
+           CALL errore('ioname','Unknown DATA type in input',1)
+      END SELECT
+
+      filename = TRIM(path_)//TRIM(prefix)//TRIM(postfix_)//TRIM(suffix_)
+
+  END SUBROUTINE ioname
+   
+
+!**********************************************************
+   SUBROUTINE read_iodata(unit,name,prefix_,postfix_,work_dir_,title_,found)
+   !**********************************************************
+      IMPLICIT NONE
+      INTEGER,         INTENT(in)   :: unit
+      CHARACTER(*),    INTENT(in)   :: name
+      LOGICAL,         INTENT(out)  :: found
+      CHARACTER(*),    INTENT(out)  :: prefix_, postfix_, work_dir_, title_ 
+
+      CHARACTER(11)                 :: sub_name='read_iodata'
+      CHARACTER(nstrx)              :: attr
+      INTEGER                       :: ierr
+
+      CALL iotk_scan_empty(unit,name,FOUND=found,ATTR=attr,IERR=ierr)
+      IF ( .NOT. found ) RETURN
+      IF ( ierr > 0 ) CALL errore(sub_name,'Wrong format in tag '//TRIM(name),ierr)
+
+      CALL iotk_scan_attr(attr,'prefix',prefix_,IERR=ierr)
+         IF (ierr /= 0) CALL errore(sub_name,'Wrong input format in PREFIX',ABS(ierr))
+      CALL iotk_scan_attr(attr,'postfix',postfix_,IERR=ierr)
+         IF (ierr /= 0) CALL errore(sub_name,'Wrong input format in POSTFIX',ABS(ierr))
+      CALL iotk_scan_attr(attr,'work_dir',work_dir_,IERR=ierr)
+         IF (ierr /= 0) CALL errore(sub_name,'Wrong input format in WORK_DIR',ABS(ierr))
+      CALL iotk_scan_attr(attr,'title',title_,IERR=ierr)
+         IF (ierr /= 0) CALL errore(sub_name,'Wrong input format in TITLE',ABS(ierr))
+
+   END SUBROUTINE read_iodata
+
+
+!**********************************************************
+   SUBROUTINE write_iodata(unit,name,prefix_,postfix_,work_dir_,title_)
+   !**********************************************************
+      IMPLICIT NONE
+      INTEGER,         INTENT(in)   :: unit
+      CHARACTER(*),    INTENT(in)   :: name
+      CHARACTER(*),    INTENT(in)   :: prefix_, postfix_, work_dir_, title_ 
+
+      CHARACTER(nstrx)              :: attr
+
+      CALL iotk_write_attr(attr,"prefix",TRIM(prefix_),FIRST=.TRUE.)
+      CALL iotk_write_attr(attr,"postfix",TRIM(postfix_),FIRST=.TRUE.)
+      CALL iotk_write_attr(attr,"work_dir",TRIM(work_dir_))
+      CALL iotk_write_attr(attr,"title",TRIM(title_))
+      CALL iotk_write_empty(unit,name,ATTR=attr)
+
+   END SUBROUTINE write_iodata
+
+!
+! Copyright (C) 2002 FPMD & PWSCF group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+     !
+     !-----------------------------------------------------------------------
+     SUBROUTINE io_global_start( mpime, ionode_set )
+       !-----------------------------------------------------------------------
+       !
+       IMPLICIT NONE
+       !
+       INTEGER, INTENT(IN) :: mpime, ionode_set
+       !
+       !
+       IF ( mpime == ionode_set ) THEN
+          ionode = .TRUE.
+       ELSE
+          ionode = .FALSE.
+       END IF
+       !
+       ionode_id = ionode_set
+       first = .FALSE.
+       !
+       RETURN
+       !
+     END SUBROUTINE io_global_start
+     !
+     !
+     !-----------------------------------------------------------------------
+     SUBROUTINE io_global_getionode( ionode_out, ionode_id_out )
+       !-----------------------------------------------------------------------
+       !
+       IMPLICIT NONE
+       !
+       LOGICAL, INTENT(OUT) :: ionode_out
+       INTEGER, INTENT(OUT) :: ionode_id_out
+       !
+       !
+       IF ( first ) &
+          CALL errore( ' get_ionode ', ' ionode not yet defined ', 1 )
+       ionode_out = ionode
+       ionode_id_out = ionode_id
+       !
+       RETURN
+       !
+     END SUBROUTINE io_global_getionode
+
+END MODULE io_module
+
