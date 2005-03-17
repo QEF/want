c507b27a39c85df8e098cc7bf7f8654c51ea242d
arrigo <arrigo@0e440500-e291-4b27-a0db-898e6d94e59d>
2005-03-17 19:41:44 +0000

    updated configure from espresso split moduledep.sh into two files [Gerardo]
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@246 0e440500-e291-4b27-a0db-898e6d94e59d

--- space_invaders/makedeps.sh
+++ space_invaders/makedeps.sh
@@ -1,30 +1,39 @@
 #!/bin/sh
-# compute dependencies for the PWscf directory tree
+# compute dependencies for the WanT directory tree
 
 # run from directory where this script is
 cd `echo $0 | sed 's/\(.*\)\/.*/\1/'` # extract pathname
 TOPDIR=`pwd`
 
-for DIR in IOTK_lib Modules Libs Main 
+for DIR in IOTK_lib Modules Libs Main Transport
 do
     # set inter-directory dependencies
     case $DIR in
-        IOTK_lib )    DEPENDS=""                        ;;
-        Modules )     DEPENDS="../IOTK_lib ../Include"  ;;
-        Libs | Main ) DEPENDS="../Modules ../Include ../IOTK_lib"  ;;
+        IOTK_lib ) DEPENDS="../Include"                        ;;
+        Modules )  DEPENDS="../Include ../IOTK_lib"            ;;
+        Libs | Main | Transport )
+                   DEPENDS="../Include ../IOTK_lib ../Modules" ;;
     esac
 
     # generate dependencies file
     if test -d $TOPDIR/$DIR
     then
         cd $TOPDIR/$DIR
-        $TOPDIR/moduledep.sh $DEPENDS > .dependencies
+        $TOPDIR/moduledep.sh $DEPENDS > make.depend
+        $TOPDIR/includedep.sh $DEPENDS >> make.depend
+    fi
+
+    # special case: Libs/fft_stick.c includes Libs/fftw.c
+    if test "$DIR" = "Libs"
+    then
+        mv make.depend make.depend.tmp
+        sed 's/@fftw.c@/fftw.c/' make.depend.tmp > make.depend
+        rm -f make.depend.tmp
     fi
 
     # check for missing dependencies
-    if grep @ .dependencies
+    if grep @ make.depend
     then
         echo WARNING: modules not found in directory $DIR
     fi
 done
-
