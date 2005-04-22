5cef116a7a235eacb21ad5ce4f2d2f7dac26dd70
arrigo <arrigo@0e440500-e291-4b27-a0db-898e6d94e59d>
2005-04-22 17:23:17 +0000

    ottimizzazioni in corso di wannier.x : aggiunte temporizzazioni in parti importanti del codice... routine omega.f90 parzialmente ottimizzata..
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@280 0e440500-e291-4b27-a0db-898e6d94e59d

--- space_invaders/Libs/omega.f90
+++ space_invaders/Libs/omega.f90
@@ -8,297 +8,206 @@
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-!=-------------------------------------------------------------------------------------=
-      SUBROUTINE omega( nbands, nkpts, nkpts2, nntot, nnmx, nnlist, bk, wb, cm,           &
-                 csheet, sheet, rave, r2ave, rave2, func_om1, func_om2, func_om3, func_o, &
-                 rtot, r2tot, func_i, func_d, func_od, func_v )
-!=--------------------------------------------------------------------------------------=
-      
-      USE kinds
-      USE constants, ONLY : ZERO, ONE, CI 
-      USE io_module, ONLY : stdout
+!********************************************************
+   SUBROUTINE omega( dimwann, nkpts, Mkb, csheet, sheet,  &
+                     rave, r2ave, rave2, func_om1, func_om2, func_om3 )
+   !********************************************************
+   USE kinds
+   USE constants, ONLY : ZERO, ONE, CI 
+   USE io_module, ONLY : stdout
+   USE timing_module, ONLY : timing
+   USE kpoints_module, ONLY : nnx, nntot, nnlist, bk, wb
+   USE localization_module, ONLY : Omega_I, Omega_OD, Omega_D, Omega_V, Omega_tot
+   IMPLICIT NONE
 
-      IMPLICIT NONE
+   !
+   ! input variables
+   !
+   INTEGER,      INTENT(in) :: dimwann, nkpts
+   REAL(dbl),    INTENT(in) :: sheet(dimwann,nkpts,*)
+   COMPLEX(dbl), INTENT(in) :: Mkb(dimwann,dimwann,nnx,nkpts)
+   COMPLEX(dbl), INTENT(in) :: csheet(dimwann,nkpts,*)
 
-      INTEGER :: nbands
-      INTEGER :: nkpts 
-      INTEGER :: nkpts2 
-      INTEGER :: nnmx 
-      INTEGER :: nwann
-      INTEGER :: ind
-      INTEGER :: nkp
-      INTEGER :: nn
-      INTEGER :: i, m, n 
+   REAL(dbl), INTENT(out) :: r2ave(dimwann), rave2(dimwann), rave(3,dimwann)
+   REAL(dbl), INTENT(out) :: func_om1, func_om2, func_om3 
+
+   !
+   ! local variables
+   !
+   REAL(dbl) :: aux(3), rtmp
+   INTEGER :: iwann, ik, inn
+   INTEGER :: i, m, n 
+   !
+   ! end of declariations
+   !
+
+!
+!------------------------------
+! main body 
+!------------------------------
+!
+      CALL timing('omega',OPR='start')
 
 
-      INTEGER :: nnlist(nkpts,nnmx)
-      INTEGER :: nntot(nkpts)
-      REAL(dbl) :: wb(nkpts,nnmx)
-      REAL(dbl) :: bk(3,nkpts,nnmx), rave(3,nbands), rtot(3)
-      REAL(dbl) :: r2ave(nbands), rave2(nbands), bim(3)
-      REAL(dbl) :: sheet(nbands,nkpts,nnmx)
-      COMPLEX(dbl) :: csumt(3)
-      COMPLEX(dbl) :: cm(nbands,nbands,nnmx,nkpts)
+!
+! computing wannier centers and spreads
+!
+      !
+      ! compute <r(i)_n> = -1/Nk \Sum{k,b} wb * b(i) * Im Mkb(n,n)
+      !
+      DO iwann = 1, dimwann
+      DO i = 1, 3
+           rave(i,iwann) = ZERO
 
-      COMPLEX(dbl) :: csheet(nbands,nkpts,nnmx)
-      COMPLEX(dbl) :: craven(3,nbands), crtotn(3)
-      REAL(dbl) :: r2aven(nbands), rave2n(nbands)
+           DO ik = 1, nkpts
+           DO inn = 1, nntot(ik)
+                 rave(i,iwann) = rave(i,iwann) + wb(ik,inn) * bk(i,ik,inn) *        &
+                     ( AIMAG(LOG( csheet(iwann,ik,inn) * Mkb(iwann,iwann,inn,ik) ) ) &
+                     - sheet(iwann,ik,inn) )
+           ENDDO
+           ENDDO
+           rave(i,iwann) = - rave(i,iwann) / DBLE(nkpts)
+      ENDDO
+      ENDDO
 
-      REAL(dbl) :: func_om1 
-      REAL(dbl) :: func_om2 
-      REAL(dbl) :: func_om3 
-      REAL(dbl) :: func_i
-      REAL(dbl) :: func_od
-      REAL(dbl) :: func_d 
-      REAL(dbl) :: func_o
-      REAL(dbl) :: func_v
-      REAL(dbl) :: func_om
-      REAL(dbl) :: r2tot
-      REAL(dbl) :: sqim 
-      REAL(dbl) :: bim2
-      REAL(dbl) :: func_n 
-      REAL(dbl) :: sum 
-      REAL(dbl) :: brn
+      !
+      ! compute <r>^2
+      !
+      DO iwann = 1, dimwann
+          rave2(iwann) = rave(1,iwann)**2 + rave(2,iwann)**2 + rave(3,iwann)**2
+      ENDDO
 
-      DO nwann = 1, nbands
-        DO ind = 1, 3
-          rave(ind,nwann) = ZERO
-          DO nkp = 1, nkpts2
-            DO nn = 1, nntot(nkp)
-              rave(ind,nwann) = rave(ind,nwann) + wb(nkp,nn) * bk(ind,nkp,nn) *      &
-                  ( AIMAG(LOG( csheet(nwann,nkp,nn) * cm(nwann,nwann,nn,nkp) ) )     &
-                   - sheet(nwann,nkp,nn) )
-            END DO
-          END DO
-          rave(ind,nwann) = -rave(ind,nwann) / DBLE(nkpts2)
-        END DO
-      END DO
+      !
+      ! compute <r^2_n> = -1/Nk \Sum{k,b} wb * [ 1 - |Mkb(n,n)|^2 + ( Im Log (Mkb(n,n)) )^2 ]
+      !
+      DO iwann = 1, dimwann
+           r2ave(iwann) = ZERO
 
-      DO nwann = 1, nbands
-        rave2(nwann) = ZERO
-        DO ind = 1, 3
-          rave2(nwann) = rave2(nwann) + rave(ind,nwann)**2
-        END DO
-      END DO
-      DO ind = 1, 3
-        rtot(ind) = ZERO
-        DO nwann = 1, nbands
-          rtot(ind) = rtot(ind) + rave(ind,nwann)
-        END DO
-      END DO
+           DO ik = 1, nkpts
+           DO inn = 1, nntot(ik)
+               r2ave(iwann) = r2ave(iwann) + wb(ik,inn) * ( ONE -                     &
+                      Mkb(iwann,iwann,inn,ik) * CONJG( Mkb(iwann,iwann,inn,ik) ) +      &
+                    ( AIMAG( LOG( csheet(iwann,ik,inn) * Mkb(iwann,iwann,inn,ik) ) )   &
+                     -sheet(iwann,ik,inn) )**2 )
+           ENDDO
+           ENDDO
+           r2ave(iwann) = r2ave(iwann) / DBLE(nkpts)
+      ENDDO
 
-      DO nwann = 1, nbands
-        r2ave(nwann) = ZERO
-        DO nkp = 1, nkpts2
-          DO nn = 1, nntot(nkp)
-            r2ave(nwann) = r2ave(nwann) + wb(nkp,nn) * ( ONE -                       &
-                 cm(nwann,nwann,nn,nkp) * CONJG( cm(nwann,nwann,nn,nkp) ) +          &
-                 ( AIMAG( LOG( csheet(nwann,nkp,nn) * cm(nwann,nwann,nn,nkp) ) ) -   &
-                 sheet(nwann,nkp,nn) )**2 )
-          END DO
-        END DO
-        r2ave(nwann) = r2ave(nwann) / DBLE(nkpts2)
-      END DO
 
-      r2tot = ZERO
-      DO nwann=1,nbands
-        r2tot = r2tot + r2ave(nwann) - rave2(nwann)
-      END DO
+!
+! computing the functionals
+!
 
-      DO nwann = 1, nbands
-        DO ind = 1, 3
-          craven(ind,nwann) = ZERO
-          DO nkp = 1, nkpts2
-            DO nn = 1, nntot(nkp)
-              craven(ind,nwann) = craven(ind,nwann) + wb(nkp,nn) * & 
-                                  bk(ind,nkp,nn) * cm(nwann,nwann,nn,nkp)
-            END DO
-          END DO
-          craven(ind,nwann) = CI * craven(ind,nwann) / DBLE(nkpts2)
-        END DO
-      END DO
-
-      DO nwann = 1, nbands
-        rave2n(nwann) = ZERO
-        DO ind = 1, 3
-          rave2n(nwann) = rave2n(nwann) + craven(ind,nwann) * CONJG( craven(ind,nwann) )
-        END DO
-      END DO
-
-      DO ind = 1, 3
-        crtotn(ind) = ZERO
-        DO nwann = 1, nbands
-          crtotn(ind) = crtotn(ind) + craven(ind,nwann)
-        END DO
-      END DO
-      
-
-      func_om = ZERO
-      DO nwann = 1, nbands
-        rave2(nwann) = ZERO
-        DO i = 1, 3
-          rave2(nwann) = rave2(nwann) + rave(i,nwann) * rave(i,nwann)
-        END DO
-        func_om = func_om + r2ave(nwann) - rave2(nwann)
-      END DO
-
+      !
+      ! func_OM1 = 1/Nk \Sum_{ik, nn} \Sum_i   wb(ik,nn) * ( 1 - | Mkb(i,i,nn,ik) |^2 )
+      !
       func_om1 = ZERO
-      DO nkp = 1, nkpts2
-        DO nn = 1, nntot(nkp)
-          DO nwann = 1, nbands
-            func_om1 = func_om1 + wb(nkp,nn) *                                    &
-            ( ONE - cm(nwann,nwann,nn,nkp) * CONJG( cm(nwann,nwann,nn,nkp) ) )
-          END DO
-        END DO
-      END DO
-      func_om1 = func_om1 / DBLE(nkpts2)
+      DO ik = 1, nkpts
+      DO inn = 1, nntot(ik)
+          DO iwann = 1, dimwann
+              func_om1 = func_om1 + wb(ik,inn) *                                    &
+                 ( ONE - REAL( Mkb(iwann,iwann,inn,ik) * CONJG(Mkb(iwann,iwann,inn,ik)) ) )
+          ENDDO
+      ENDDO
+      ENDDO
+      func_om1 = func_om1 / DBLE(nkpts)
 
+
+      !
+      ! func_OM2 = 1/Nk \Sum_{ik, nn} \Sum_i   wb(ik,nn) * Im( Log( csheet * Mkb) - sheet )
+      !
       func_om2 = ZERO
-      DO nwann = 1, nbands
-        sqim = ZERO
-        DO nkp = 1, nkpts2
-          DO nn = 1, nntot(nkp)
-            sqim = sqim + wb(nkp,nn) * ( ( AIMAG( LOG( csheet(nwann,nkp,nn) *     &
-            cm(nwann,nwann,nn,nkp) ) ) - sheet(nwann,nkp,nn) )**2 )
-          END DO
-        END DO
-        sqim = sqim / DBLE(nkpts2)
-        func_om2 = func_om2 + sqim
-      END DO
+      DO ik = 1, nkpts
+      DO inn = 1, nntot(ik)
+          DO iwann = 1, dimwann
+               func_om2 = func_om2 + wb(ik,inn) * ( ( AIMAG( LOG( csheet(iwann,ik,inn) * &
+                          Mkb(iwann,iwann,inn,ik) ) ) - sheet(iwann,ik,inn) )**2 )
+          ENDDO
+      ENDDO
+      ENDDO
+      func_om2 = func_om2 / DBLE(nkpts)
 
+
+      !
+      ! func_OM3 = 1/Nk \Sum_i  \Sum_{ik, nn}  wb(ik,nn) * | bk |^2 
+      !
       func_om3 = ZERO
-      DO nwann = 1, nbands
-        DO ind = 1, 3
-          bim(ind)= ZERO
-          DO nkp = 1, nkpts2
-            DO nn = 1, nntot(nkp)
-              bim(ind) = bim(ind) + wb(nkp,nn) * bk(ind,nkp,nn) *             &
-              ( AIMAG( LOG( csheet(nwann,nkp,nn) * cm(nwann,nwann,nn,nkp) ) ) &
-               -sheet(nwann,nkp,nn) )
-            END DO
-          END DO
-          bim(ind) = bim(ind) / DBLE(nkpts2)
-        END DO
-        bim2 = ZERO
-        DO ind = 1, 3
-          bim2 = bim2 + bim(ind) * bim(ind)
-        END DO
-        func_om3 = func_om3 - bim2
-      END DO
+      DO iwann = 1, dimwann
+           DO i = 1, 3
+               aux(i)= ZERO
+               DO ik = 1, nkpts
+               DO inn = 1, nntot(ik)
+                   aux(i) = aux(i) + wb(ik,inn) * bk(i,ik,inn) *             &
+                            ( AIMAG( LOG( csheet(iwann,ik,inn) * Mkb(iwann,iwann,inn,ik) )) &
+                            - sheet(iwann,ik,inn) )
+               ENDDO
+               ENDDO
+           ENDDO
+           func_om3 = func_om3 - ( aux(1)**2 + aux(2)**2 + aux(3)**2 )
+      ENDDO
+      func_om3 = func_om3 / DBLE(nkpts)**2
 
-      func_n = ZERO
-      DO m = 1, nbands
-        DO n= 1, nbands
-          IF (n /= m ) THEN
-            DO nkp = 1, nkpts2
-              DO nn = 1, nntot(nkp)
-                func_n = func_n + wb(nkp,nn) * CONJG( cm(m,n,nn,nkp) ) * cm(m,n,nn,nkp)
-              END DO
-            END DO
-          END IF
-        END DO
-      END DO
-      func_n = func_n / DBLE(nkpts2)
 
-      func_n = ZERO
-      DO m = 1, nbands
-        DO n = 1, nbands
-          IF ( n /= m ) THEN
-            DO nkp = 1, nkpts2
-              sum = ZERO
-              DO ind = 1, 3
-                csumt(ind) = ZERO
-                DO nn= 1, nntot(nkp)
-                  csumt(ind) = csumt(ind)+ wb(nkp,nn) * bk(ind,nkp,nn) * cm(m,n,nn,nkp)
-                END DO
-                sum = sum + csumt(ind) * CONJG( csumt(ind) )
-              END DO
-              func_n = func_n + sum
-            END DO
-          END IF
-        END DO
-      END DO
-      func_n = func_n / DBLE(nkpts2)
-      
+      !
+      ! Omega_I
+      !
+      Omega_I = ZERO
+      DO ik = 1, nkpts
+      DO inn = 1, nntot(ik)
+          rtmp = ZERO
+          DO n = 1, dimwann
+          DO m = 1, dimwann
+              rtmp = rtmp + Mkb(m,n,inn,ik) * CONJG( Mkb(m,n,inn,ik) )
+          ENDDO
+          ENDDO
+          Omega_I = Omega_I + wb(ik,inn) * ( DBLE(dimwann)- rtmp )
+      ENDDO
+      ENDDO
+      Omega_I = Omega_I / DBLE(nkpts)
 
-      func_n = ZERO
-      DO m = 1, nbands
-        DO n= 1, nbands
-          DO nkp = 1, nkpts2
-            sum = ZERO
-            DO ind = 1, 3
-              csumt(ind) = ZERO
-              DO nn = 1, nntot(nkp)
-                IF ( m ==n ) THEN
-                  csumt(ind) = csumt(ind) + wb(nkp,nn) * bk(ind,nkp,nn) * CI * &
-     &            ( AIMAG( LOG( csheet(n,nkp,nn) * cm(n,n,nn,nkp) ) ) - sheet(n,nkp,nn) )
-                ELSE
-                  csumt(ind) = csumt(ind) + wb(nkp,nn) * bk(ind,nkp,nn) * cm(m,n,nn,nkp)
-                END IF
-              END DO
-              sum = sum + csumt(ind) * CONJG(csumt(ind))
-            END DO
-            func_n = func_n + sum
-          END DO
-        END DO
-      END DO
-      func_n = func_n / DBLE(nkpts2)
 
-      func_i = ZERO
-      DO n = 1, nbands
-        func_n = func_n - rave2(n)
-        func_i = func_i + r2ave(n)
-      END DO
-      func_i = func_i - func_n
+      !
+      ! func_OD
+      !
+      Omega_OD = ZERO
+      DO ik = 1, nkpts
+      DO inn = 1, nntot(ik)
+          DO n = 1, dimwann
+              DO m = 1, dimwann
+                 Omega_OD = Omega_OD + wb(ik,inn) * Mkb(m,n,inn,ik)*CONJG( Mkb(m,n,inn,ik) )
+              ENDDO
+              !
+              ! eliminate the diagonal term
+              Omega_OD = Omega_OD - wb(ik,inn) * Mkb(n,n,inn,ik) * CONJG( Mkb(n,n,inn,ik) )
+          ENDDO
+      ENDDO
+      ENDDO
+      Omega_OD = Omega_OD / DBLE(nkpts)
 
-      func_i = ZERO
-      DO nkp = 1, nkpts2
-        DO nn = 1, nntot(nkp)
-          sum = ZERO
-          DO m = 1, nbands
-            DO n = 1, nbands
-              sum = sum + cm(m,n,nn,nkp) * CONJG( cm(m,n,nn,nkp) )
-            END DO
-          END DO
-          func_i = func_i + wb(nkp,nn) * ( DBLE(nbands)-sum )
-        END DO
-      END DO
-      func_i = func_i / DBLE(nkpts2)
 
-      func_od = ZERO
-      DO nkp = 1, nkpts2
-        DO nn = 1, nntot(nkp)
-          sum = ZERO
-          DO m = 1, nbands
-            DO n = 1, nbands
-              sum = sum + wb(nkp,nn) * cm(m,n,nn,nkp) * CONJG( cm(m,n,nn,nkp) )
-              IF ( m == n ) sum = sum - wb(nkp,nn) * cm(m,n,nn,nkp) * CONJG( cm(m,n,nn,nkp) )
-            END DO
-          END DO
-          func_od = func_od + sum
-        END DO
-      END DO
-      func_od = func_od / DBLE(nkpts2)
+      !
+      ! func_D
+      !
+      Omega_D = ZERO
+      DO ik = 1, nkpts
+      DO inn = 1, nntot(ik)
+          DO n = 1, dimwann
+               rtmp = DOT_PRODUCT( bk(:, ik, inn), rave(:, n) )
+               Omega_D = Omega_D + wb(ik,inn) *  &
+                               ( AIMAG( LOG( csheet(n,ik,inn) * Mkb(n,n,inn,ik) ) ) &
+                               - sheet(n,ik,inn) + rtmp )**2
+          ENDDO
+      ENDDO
+      ENDDO
+      Omega_D = Omega_D / DBLE(nkpts)
 
-      func_d = ZERO
-      DO nkp = 1, nkpts2
-        DO nn = 1, nntot(nkp)
-          sum = ZERO
-          DO n = 1, nbands
-            brn = ZERO
-            DO ind = 1, 3
-              brn = brn + bk(ind,nkp,nn) * rave(ind,n)
-            END DO
-            sum = sum + wb(nkp,nn) * ( AIMAG( LOG( csheet(n,nkp,nn) * cm(n,n,nn,nkp) ) ) &
-                  - sheet(n,nkp,nn) + brn )**2
-          END DO
-          func_d=func_d+sum
-        END DO
-      END DO
-      func_d = func_d / DBLE(nkpts2)
 
-      func_o = func_i + func_d + func_od
-      func_v = func_d + func_od
+      Omega_tot = Omega_I + Omega_D + Omega_OD
+      Omega_V = Omega_D + Omega_OD
 
-      RETURN
-      END SUBROUTINE
+
+      CALL timing('omega',OPR='stop')
+   END SUBROUTINE omega
+
+
