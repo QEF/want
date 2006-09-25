3e4ebd27c6cae3cb7d6743ef77cce7cc6586e8dd
arrigo <arrigo@0e440500-e291-4b27-a0db-898e6d94e59d>
2006-09-25 14:09:32 +0000

    new script lib used in test06 (Au case only) as an alpha implementation; input changed accordingly
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@607 0e440500-e291-4b27-a0db-898e6d94e59d

--- want/tests/test06/run_Au.sh
+++ want/tests/test06/run_Au.sh
@@ -34,12 +34,15 @@ MANUAL=" Usage
 #
 
 #
-# source common enviroment, to be set before running the script
+# source common enviroment
 . ../environment.conf
-. $UTILITY_BIN/basedef.sh
-TEST_HOME=$(pwd)
-TEST_NAME=$(echo $TEST_HOME | awk -v FS=\/ '{print $NF}' )
-PSEUDO_NAME=Au11pw91.mt.UPF
+#
+# source low level macros for test
+. ../../script/libtest.sh
+
+#
+# macros
+SUFFIX="_Au"
 
 #
 # evaluate the starting choice about what is to run 
@@ -85,26 +88,14 @@ esac
 #
 if [ "$PLOT_SWITCH" = "no" ] ; then PLOT=".FALSE." ; fi
 
+
 #
-# preliminaries
+# initialize 
 #
 if [ -z "$CLEAN" ] ; then
-   test -e $TMPDIR/$TEST_NAME || mkdir $TMPDIR/$TEST_NAME 
-   cd $TMPDIR/$TEST_NAME
-   ln -sf $TEST_HOME/../pseudo/$PSEUDO_NAME .
-   #
-   test -e $TEST_HOME/SCRATCH  && rm $TEST_HOME/SCRATCH
-   cd $TEST_HOME
-   ln -sf $TMPDIR/$TEST_NAME ./SCRATCH
-   #
-   test -e $TMPDIR/$TEST_NAME/HOME && rm $TMPDIR/$TEST_NAME/HOME
-   cd $TMPDIR/$TEST_NAME
-   ln -sf $TEST_HOME ./HOME
-   #
-   test -e $TMPDIR/$TEST_NAME/CRASH && rm $TMPDIR/$TEST_NAME/CRASH
-
-   cd $TMPDIR/$TEST_NAME
+   test_init 
 fi
+#
 
 
 #-----------------------------------------------------------------------------
@@ -113,106 +104,56 @@ fi
 # running DFT SCF
 #
 if [ "$SCF" = ".TRUE." ] ; then  
-   echo $ECHO_N "running SCF calculation... $ECHO_C" 
-   $PARA_PREFIX  $DFT_BIN/pw.x $PARA_POSTFIX < $TEST_HOME/scf_Au.in > $TEST_HOME/scf_Au.out
-   if [ $? = 0 ] ; then 
-      echo "$ECHO_T done" 
-   else
-      echo "$ECHO_T problems found" ; exit 1
-   fi
+   run_dft  NAME=SCF INPUT=scf$SUFFIX.in  OUTPUT=scf$SUFFIX.out
 fi
 
 #
 # running DFT NSCF
 #
 if [ "$NSCF" = ".TRUE." ] ; then  
-   echo $ECHO_N "running NSCF calculation... $ECHO_C" 
-   $PARA_PREFIX  $DFT_BIN/pw.x $PARA_POSTFIX < $TEST_HOME/nscf_Au.in > $TEST_HOME/nscf_Au.out
-   if [ $? = 0 ] ; then 
-      echo "$ECHO_T done" 
-   else
-      echo "$ECHO_T problems found" ; exit 1
-   fi
+   run_dft  NAME=NSCF INPUT=nscf$SUFFIX.in  OUTPUT=nscf$SUFFIX.out
 fi
    
 #
 # running DFT PWEXPORT
 #
 if [ "$PWEXPORT" = ".TRUE." ] ; then  
-   echo "running PWEXPORT calculation..." 
-   $PARA_PREFIX  $DFT_BIN/pw_export.x $PARA_POSTFIX  \
-              <  $TEST_HOME/pwexport_Au.in > $TEST_HOME/pwexport_Au.out
-   if [ $? = 0 ] ; then 
-      echo "done" 
-   else
-      echo "problems found" ; exit 1
-   fi
+   run_export  INPUT=pwexport$SUFFIX.in  OUTPUT=pwexport$SUFFIX.out
 fi
 
 #
 # running DISENTANGLE
 #
 if [ "$DISENTANGLE" = ".TRUE." ] ; then  
-   echo $ECHO_N "running DISENTANGLE calculation... $ECHO_C" 
-   $WANT_BIN/disentangle.x < $TEST_HOME/want_Au.in > $TEST_HOME/disentangle_Au.out
-   if [ ! -e CRASH ] ; then 
-      echo "$ECHO_T done" 
-   else
-      echo "$ECHO_T problems found" ; cat CRASH ; exit 1
-   fi
+   run_disentangle  INPUT=want$SUFFIX.in  OUTPUT=disentangle$SUFFIX.out
 fi
 
 #
 # running WANNIER
 #
 if [ "$WANNIER" = ".TRUE." ] ; then  
-   echo $ECHO_N "running WANNIER calculation... $ECHO_C" 
-   $WANT_BIN/wannier.x < $TEST_HOME/want_Au.in > $TEST_HOME/wannier_Au.out
-   if [ ! -e CRASH ] ; then 
-      echo "$ECHO_T done" 
-   else
-      echo "$ECHO_T problems found" ; cat CRASH ; exit 1
-   fi
+   run_wannier  INPUT=want$SUFFIX.in  OUTPUT=wannier$SUFFIX.out
 fi
 
 #
 # running BANDS
 #
 if [ "$BANDS" = ".TRUE." ] ; then  
-   echo $ECHO_N "running BANDS calculation... $ECHO_C" 
-   $WANT_BIN/bands.x < $TEST_HOME/bands_Au.in > $TEST_HOME/bands_Au.out
-   if [ ! -e CRASH ] ; then 
-      echo "$ECHO_T done" 
-   else
-      echo "$ECHO_T problems found" ; cat CRASH ; exit 1
-   fi
+   run_bands  INPUT=bands$SUFFIX.in  OUTPUT=bands$SUFFIX.out
 fi
 
 #
 # running DOS
 #
 if [ "$DOS" = ".TRUE." ] ; then  
-   echo $ECHO_N "running DOS calculation... $ECHO_C" 
-   $WANT_BIN/dos.x < $TEST_HOME/dos_Au.in > $TEST_HOME/dos_Au.out
-   if [ ! -e CRASH ] ; then 
-      echo "$ECHO_T done" 
-      test -e dos_Au.dat  &&  mv dos_Au.dat  $TEST_HOME/dos_Au.dat
-   else
-      echo "$ECHO_T problems found" ; cat CRASH ; exit 1
-   fi
+   run_dos  INPUT=dos$SUFFIX.in  OUTPUT=dos$SUFFIX.out
 fi
 
 #
 # running PLOT
 #
 if [ "$PLOT" = ".TRUE." ] ; then  
-   echo $ECHO_N "running PLOT calculation... $ECHO_C" 
-   $WANT_BIN/plot.x < $TEST_HOME/plot_Au.in > $TEST_HOME/plot_Au.out
-   if [ ! -e CRASH ] ; then 
-      echo "$ECHO_T done" 
-   else
-      echo "$ECHO_T problems found" ; cat CRASH ; exit 1
-   fi
+   run_plot  INPUT=plot$SUFFIX.in  OUTPUT=plot$SUFFIX.out
 fi
 
 #
@@ -220,15 +161,13 @@ fi
 #
 if [ "$CONDUCTOR" = ".TRUE." ] ; then  
    #
-   echo $ECHO_N "running CONDUCTOR calculation... $ECHO_C" 
-   $WANT_BIN/conductor.x < $TEST_HOME/conductor_Au.in > $TEST_HOME/conductor_Au.out
+   run_conductor INPUT=conductor$SUFFIX.in OUTPUT=conductor$SUFFIX.out
+   #
    if [ ! -e CRASH ] ; then 
       echo "$ECHO_T done" 
       #
-      test -e doscond.dat  &&  mv doscond.dat  $TEST_HOME/doscond_Au.dat
-      test -e cond.dat     &&  mv cond.dat     $TEST_HOME/cond_Au.dat
-   else
-      echo "$ECHO_T problems found" ; cat CRASH ; exit 1
+      test -e doscond.dat  &&  mv doscond.dat  $TEST_HOME/doscond$SUFFIX.dat
+      test -e cond.dat     &&  mv cond.dat     $TEST_HOME/cond$SUFFIX.dat
    fi
 fi
 
@@ -239,11 +178,11 @@ if [ "$CHECK" = ".TRUE." ] ; then
    echo "running CHECK... "
    #
    cd $TEST_HOME
-   list="disentangle_Au.out wannier_Au.out"
+   list="disentangle$SUFFIX.out wannier$SUFFIX.out"
    #
    for file in $list
    do
-      $UTILITY_BIN/check.sh $file
+      ../../script/check.sh $file
    done
 fi
 
@@ -252,12 +191,7 @@ fi
 # eventually clean
 #
 if [ "$CLEAN" = ".TRUE." ] ; then  
-   cd $TEST_HOME
-      rm -rf *.out *.dat 2> /dev/null
-      test -e SCRATCH && rm SCRATCH
-   cd $TMPDIR
-      test -d $TEST_NAME && rm -rf $TEST_NAME
-   exit 0
+   run_clean
 fi
 
 #
