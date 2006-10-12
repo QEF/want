eb8af90c1ac4cbefddc579334b86dc8528a5edd6
arrigo <arrigo@0e440500-e291-4b27-a0db-898e6d94e59d>
2006-10-12 11:14:27 +0000

    ctools dir added; c routines moved here; memstat.c added ; AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk/want@624 0e440500-e291-4b27-a0db-898e6d94e59d

---
+++ ctools/memstat.c
@@ -0,0 +1,27 @@
+/*
+  Copyright (C) 2002 FPMD group
+  This file is distributed under the terms of the
+  GNU General Public License. See the file `License'
+  in the root directory of the present distribution,
+  or http://www.gnu.org/copyleft/gpl.txt .
+*/
+
+#include "ctools.h"
+
+/* 
+  This function return the numer of kilobytes allocated
+  by the calling process. 
+  Auhor: Carlo Cavazzoni.
+*/
+
+void FC_FUNC(memstat,MEMSTAT)(int *kilobytes)
+{
+#if defined(HAVE_MALLINFO)
+#include <malloc.h>
+  struct mallinfo info;  
+  info = mallinfo();
+  *kilobytes = info.arena / 1024 ;
+#else
+  *kilobytes = -1;
+#endif
+}
