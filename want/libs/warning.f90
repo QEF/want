587fba026afc74537740c399cfc35217cfa7491a
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2008-06-12 12:52:19 +0000

    10 Jun 2008      bug fixed when reading US pseudopot. not in UPF format.                  The global management of PP has been updated according to                  espresso-4.0: major changes occured. 17 Apr 2008      iotk lib updated to v1.2.0 (fro the head of toady's CVS).                  qexml.f90 further updated to include the very last changes to                  qexml fmt before the release of espresso-4.0 . (AF)
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@997 0e440500-e291-4b27-a0db-898e6d94e59d

--- want/libs/warning.f90
+++ want/libs/warning.f90
@@ -5,56 +5,31 @@
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-! based on the error.f90 file from Quantum-Espresso
+! based on the infomsg routine from Quantum-Espresso
 !
-!*****************************************
-   SUBROUTINE warning( ounit, msg)
-   !*****************************************
-   !
-   !  this routine a warning message
-   !  INPUT: msg       the messageto be printed
-   !
-   IMPLICIT NONE
-
-#if defined __MPI
-   include 'mpif.h'
-#endif
-
-   !
-   ! input variables
-   !
-   INTEGER                       :: ounit
-   CHARACTER(LEN=*), INTENT(in)  :: msg
-
-   !
-   ! local variables
-   !
-   INTEGER :: ip, nproc, mpime
-   !
-#if defined __MPI
-   INTEGER :: ierr
-#endif
-
-
-!
-!----------------------------------------
-! main Body
-!----------------------------------------
-!
-#if defined __MPI
-      CALL mpi_comm_size(mpi_comm_world,nproc,ierr)
-      CALL mpi_comm_rank(mpi_comm_world,mpime,ierr)
-#else
-      mpime = 0
-      nproc = 1
-#endif
-
-!
-! print the warning message
-!
-      DO ip = 0, nproc-1
-          IF( mpime == 0 ) WRITE ( ounit,"(2x,'WARNING: ',a)") TRIM(msg) 
-      ENDDO
-
+!----------------------------------------------------------------------
+SUBROUTINE warning( routine, message )
+  !----------------------------------------------------------------------
+  !
+  ! ... This is a simple routine which writes an info message 
+  ! ... from a given routine to output. 
+  !
+  USE io_global_module,  ONLY : stdout, ionode
+  !
+  IMPLICIT NONE
+  !
+  CHARACTER (LEN=*) :: routine, message
+  ! the name of the calling routine
+  ! the output message
+  !
+  IF ( ionode ) THEN
+     !   
+     WRITE( stdout , '(5X,"WARNING from routine ",A,":")' ) routine
+     WRITE( stdout , '(5X,A)' ) message
+     !   
+  END IF
+  !
+  RETURN
+  !
 END SUBROUTINE warning
 
