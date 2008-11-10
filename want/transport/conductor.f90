0b27afb7da2c332401505956a9fcb0d9f273f8b2
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2008-11-10 23:38:14 +0000

    I/O to stdout in parallel runs fixed. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@1066 0e440500-e291-4b27-a0db-898e6d94e59d

--- want/transport/conductor.f90
+++ want/transport/conductor.f90
@@ -306,7 +306,7 @@
 
       avg_iter = avg_iter/REAL(2*nkpts_par)
       !
-      IF ( MOD( ie, nprint) == 0 .OR.  ie == nomg_s .OR. ie == nomg_e ) THEN
+      IF ( ionode .AND. (MOD( ie, nprint) == 0 .OR.  ie == nomg_s .OR. ie == nomg_e )) THEN
           !
           WRITE(stdout,"(2x,'T matrix converged after avg. # of iterations ',f8.3,/)") &
                 avg_iter
