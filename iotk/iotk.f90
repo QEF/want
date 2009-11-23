4a001d46cf2bf699d1b3c7e8dea3718efb666674
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2009-11-23 03:38:19 +0000

    iotk library updated to todays cvs. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk/want@1226 0e440500-e291-4b27-a0db-898e6d94e59d

--- iotk/iotk.f90
+++ iotk/iotk.f90
@@ -5,33 +5,12 @@ program iotk
   use iotk_base
   use iotk_error_interf
   implicit none
-  integer :: iarg,iostat,pos,ierrl
+  integer :: nargs,ierrl
   character(iotk_linlenx) :: args(iotk_maxargs)
 
-  ierrl = 0
-  iarg = 0
-  do
-    iarg = iarg + 1
-    if(iarg>size(args)) then
-      call iotk_error_issue(ierrl,"",__FILE__,__LINE__)
-      goto 1
-    end if
-    read(*,"(a)",iostat=iostat) args(iarg)
-    if(iostat<0) exit
-    if(iostat>0) then
-      call iotk_error_issue(ierrl,"",__FILE__,__LINE__)
-      goto 1
-    end if
-    pos = scan(args(iarg),"|",back=.true.)
-    if(pos>0 .and. args(iarg)(pos:)=="|") then
-      args(iarg)(pos:pos) = iotk_eos
-    else
-      pos = len_trim(args(iarg)) + 1
-      if(pos<=len(args)) args(iarg)(pos:pos) = iotk_eos
-    end if
-  end do
-  iarg = iarg -1
-  call iotk_tool(args(1:iarg))
+  call iotk_readcmdline(args,nargs,eos=.true.,ierr=ierrl)
+  if(ierrl/=0) goto 1
+  call iotk_tool(args(1:nargs))
 1 continue
   if(ierrl/=0) call iotk_error_handler(ierrl)
 end program iotk
--- iotk/iotk_version.h
+++ iotk/iotk_version.h
@@ -1,5 +1,5 @@
-#define __IOTK_VERSION "1.2.0development"
+#define __IOTK_VERSION "1.2.0"
 #define __IOTK_VERSION_MAJOR 1
 #define __IOTK_VERSION_MINOR 2
 #define __IOTK_VERSION_PATCH 0
-#define __IOTK_VERSION_EXTRA "development"
+#define __IOTK_VERSION_EXTRA ""
