19cdb6e8588bb6f4d911aee4da6a6c64c16bed2f
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2010-04-05 21:17:56 +0000

    test largely updated (lead1 and lead4 cases added, etc). AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@1271 0e440500-e291-4b27-a0db-898e6d94e59d

--- want/tests/test11/run.sh
+++ want/tests/test11/run.sh
@@ -14,17 +14,32 @@ MANUAL=" Usage
  
  scf             DFT self-consistent calculation
  nscf            DFT non-self-consistent calculation
- pwexport        export DFT data to WanT package
- dft             perform SCF, NSCF, PWEXPORT all together
+ scf_bulk1       DFT self-consistent calculation for Pt bulk (1atom)
+ nscf_bulk1      DFT non-self-consistent calculation for Pt bulk (1atom)
+ scf_bulk4       DFT self-consistent calculation for Pt bulk (4atoms)
+ nscf_bulk4      DFT non-self-consistent calculation for Pt bulk (4atoms)
+ dft             perform all the above together
+
  disentangle     select the optimal subspace on which perform
                  the wannier minimization
  wannier         perform the above cited minimization
  plot            compute WFs on real space for plotting
- conductor       evaluate the transmittance across the junction
+ disentangle_bulk1
+ wannier_bulk1
+ disentangle_bulk4
+ wannier_bulk4   want calculations for Pt bulk (1 and 4 atoms)
+
+ conductor_lead1 evaluate the transmittance across the junction
+                 using leads with 1 aton
+ conductor_lead4 evaluate the transmittance across the junction
+                 using leads with 4 atons
  conductor_auto  evaluate the transmittance taking all the matrix elements
                  from the same calculation (not recommended in general)
- conductor_bulk  evaluate the bulk transmittance for the leads
- current         compute the current
+ conductor_bulk1 evaluate the bulk transmittance for the leads (1 atom)
+ conductor_bulk4 evaluate the bulk transmittance for the leads (4 atoms)
+ current_lead1   compute the current from the results of conductor_lead1
+ current_lead4   compute the current from the results of conductor_lead4
+
  want            perform all WANT calculations
  all             perform all the above described steps
 
@@ -49,14 +64,27 @@ SUFFIX=
 
 SCF=
 NSCF=
-PWEXPORT=
+SCF_BULK1=
+NSCF_BULK1=
+SCF_BULK4=
+NSCF_BULK4=
+
 DISENTANGLE=
 WANNIER=
 PLOT=
-CONDUCTOR=
+DISENTANGLE_BULK1=
+WANNIER_BULK1=
+DISENTANGLE_BULK4=
+WANNIER_BULK4=
+
+CONDUCTOR_BULK1=
+CONDUCTOR_BULK4=
+CONDUCTOR_LEAD1=
+CONDUCTOR_LEAD4=
 CONDUCTOR_AUTO=
-CONDUCTOR_BULK=
-CURRENT=
+CURRENT_LEAD1=
+CURRENT_LEAD4=
+
 CHECK=
 CLEAN=
 
@@ -66,22 +94,50 @@ INPUT=`echo $1 | tr [:upper:] [:lower:]`
 case $INPUT in 
    (scf)            SCF=yes ;;
    (nscf)           NSCF=yes ;;
-   (pwexport)       PWEXPORT=yes ;;
-   (dft)            SCF=yes ; NSCF=yes ; PWEXPORT=yes ;;
+   (scf_bulk1)      SCF_BULK1=yes ;;
+   (nscf_bulk1)     NSCF_BULK1=yes ;;
+   (scf_bulk4)      SCF_BULK4=yes ;;
+   (nscf_bulk4)     NSCF_BULK4=yes ;;
+
+   (dft)            SCF=yes ; NSCF=yes ; 
+                    SCF_BULK1=yes ; NSCF_BULK1=yes ;
+                    SCF_BULK4=yes ; NSCF_BULK4=yes ;;
+
    (disentangle)    DISENTANGLE=yes ;;
    (wannier)        WANNIER=yes ;;
    (plot)           PLOT=yes ;;
-   (conductor)      CONDUCTOR=yes ;;
-   (conductor_auto) CONDUCTOR_AUTO=yes ;;
-   (conductor_bulk) CONDUCTOR_BULK=yes ;;
-   (current)        CURRENT=yes ;;
-   (want)           DISENTANGLE=yes ; WANNIER=yes ; 
-                    CONDUCTOR=yes ; CONDUCTOR_AUTO=yes ; CONDUCTOR_BULK=yes ; 
-                    CURRENT=yes ; PLOT=yes ;;
-   (all)            SCF=yes ; NSCF=yes ; PWEXPORT=yes ; 
-                    DISENTANGLE=yes ; WANNIER=yes ; 
-                    CONDUCTOR=yes ; CONDUCTOR_AUTO=yes ; CONDUCTOR_BULK=yes ; 
-                    CURRENT=yes ; PLOT=yes ;;
+   (disentangle_bulk1)    DISENTANGLE_BULK1=yes ;;
+   (wannier_bulk1)        WANNIER_BULK1=yes ;;
+   (disentangle_bulk4)    DISENTANGLE_BULK4=yes ;;
+   (wannier_bulk4)        WANNIER_BULK4=yes ;;
+
+   (conductor_bulk1)      CONDUCTOR_BULK1=yes ;;
+   (conductor_bulk4)      CONDUCTOR_BULK4=yes ;;
+   (conductor_lead1)      CONDUCTOR_LEAD1=yes ;;
+   (conductor_lead4)      CONDUCTOR_LEAD4=yes ;;
+   (conductor_auto)       CONDUCTOR_AUTO=yes ;;
+   (current_lead1)        CURRENT_LEAD1=yes ;;
+   (current_lead4)        CURRENT_LEAD4=yes ;;
+ 
+   (want)           DISENTANGLE=yes ; WANNIER=yes ; PLOT=yes ;
+                    DISENTANGLE_BULK1=yes ; WANNIER_BULK1=yes ; 
+                    DISENTANGLE_BULK4=yes ; WANNIER_BULK4=yes ; 
+                    CONDUCTOR_BULK1=yes ; CONDUCTOR_BULK4=yes ; 
+                    CONDUCTOR_LEAD1=yes ; CONDUCTOR_LEAD4=yes ; 
+                    CONDUCTOR_AUTO=yes ; CONDUCTOR_BULK=yes ; 
+                    CURRENT_LEAD1=yes ; CURRENT_LEAD4=yes ;; 
+
+   (all)            SCF=yes ; NSCF=yes ; 
+                    SCF_BULK1=yes ; NSCF_BULK1=yes ;
+                    SCF_BULK4=yes ; NSCF_BULK4=yes ;
+                    DISENTANGLE=yes ; WANNIER=yes ; PLOT=yes ;
+                    DISENTANGLE_BULK1=yes ; WANNIER_BULK1=yes ; 
+                    DISENTANGLE_BULK4=yes ; WANNIER_BULK4=yes ; 
+                    CONDUCTOR_BULK1=yes ; CONDUCTOR_BULK4=yes ; 
+                    CONDUCTOR_LEAD1=yes ; CONDUCTOR_LEAD4=yes ; 
+                    CONDUCTOR_AUTO=yes ; CONDUCTOR_BULK=yes ; 
+                    CURRENT_LEAD1=yes ; CURRENT_LEAD4=yes ;; 
+
    (check)          CHECK=yes ;;
    (clean)          CLEAN=yes ;;
    (*)              echo " Invalid input FLAG, type ./run.sh for help" ; exit 1 ;;
@@ -114,10 +170,28 @@ run_dft  NAME=SCF   SUFFIX=$SUFFIX  RUN=$SCF
 #
 run_dft  NAME=NSCF  SUFFIX=$SUFFIX  RUN=$NSCF
 
+
 #
-# running DFT PWEXPORT
+# running DFT SCF BULK1
 #
-run_export  SUFFIX=$SUFFIX  RUN=$PWEXPORT
+run_dft  NAME=SCF_BULK1   SUFFIX=${SUFFIX}  RUN=$SCF_BULK1
+
+#
+# running DFT NSCF BULK1
+#
+run_dft  NAME=NSCF_BULK1   SUFFIX=${SUFFIX}  RUN=$NSCF_BULK1
+
+
+#
+# running DFT SCF BULK4
+#
+run_dft  NAME=SCF_BULK4   SUFFIX=${SUFFIX}  RUN=$SCF_BULK4
+
+#
+# running DFT NSCF BULK4
+#
+run_dft  NAME=NSCF_BULK4   SUFFIX=${SUFFIX}  RUN=$NSCF_BULK4
+
 
 
 #
@@ -137,14 +211,39 @@ run_plot  SUFFIX=$SUFFIX  RUN=$PLOT
 
 
 #
-# running CONDUCTOR
+# running DISENTANGLE BULK1
 #
-run_conductor SUFFIX=$SUFFIX  RUN=$CONDUCTOR
+run_disentangle  NAME=DISENTANGLE_BULK1  SUFFIX=${SUFFIX}_bulk1  RUN=$DISENTANGLE_BULK1
 
 #
-# running CURRENT
+# running WANNIER BULK1
 #
-run_current NAME=CURRENT  SUFFIX=$SUFFIX  RUN=$CURRENT
+run_wannier  NAME=WANNIER_BULK1  SUFFIX=${SUFFIX}_bulk1  RUN=$WANNIER_BULK1
+
+#
+# running DISENTANGLE BULK4
+#
+run_disentangle  NAME=DISENTANGLE_BULK1  SUFFIX=${SUFFIX}_bulk4 RUN=$DISENTANGLE_BULK4
+
+#
+# running WANNIER BULK4
+#
+run_wannier  NAME=WANNIER_BULK4  SUFFIX=${SUFFIX}_bulk4  RUN=$WANNIER_BULK4
+
+
+
+
+#
+# running CONDUCTOR BULK
+#
+run_conductor NAME=CONDUCTOR_BULK1  SUFFIX=$SUFFIX  RUN=$CONDUCTOR_BULK1
+run_conductor NAME=CONDUCTOR_BULK4  SUFFIX=$SUFFIX  RUN=$CONDUCTOR_BULK4
+
+#
+# running CONDUCTOR LEAD
+#
+run_conductor NAME=CONDUCTOR_LEAD1  SUFFIX=$SUFFIX  RUN=$CONDUCTOR_LEAD1
+run_conductor NAME=CONDUCTOR_LEAD4  SUFFIX=$SUFFIX  RUN=$CONDUCTOR_LEAD4
 
 #
 # running CONDUCTOR_AUTO
@@ -152,9 +251,10 @@ run_current NAME=CURRENT  SUFFIX=$SUFFIX  RUN=$CURRENT
 run_conductor NAME=CONDUCTOR_AUTO  SUFFIX=${SUFFIX}_auto  RUN=$CONDUCTOR_AUTO
 
 #
-# running CONDUCTOR_BULK
+# running CURRENT
 #
-run_conductor NAME=CONDUCTOR_BULK  SUFFIX=${SUFFIX}_bulk  RUN=$CONDUCTOR_BULK
+run_current NAME=CURRENT_LEAD1  SUFFIX=$SUFFIX  RUN=$CURRENT_LEAD1
+run_current NAME=CURRENT_LEAD4  SUFFIX=$SUFFIX  RUN=$CURRENT_LEAD4
 
 
 
@@ -165,7 +265,10 @@ if [ "$CHECK" = yes ] ; then
    echo "running CHECK..."
    #
    cd $TEST_HOME
-   list="disentangle$SUFFIX.out wannier$SUFFIX.out"
+   list="disentangle$SUFFIX.out wannier$SUFFIX.out
+         disentangle_bulk1$SUFFIX.out wannier_bulk1$SUFFIX.out
+         disentangle_bulk4$SUFFIX.out wannier_bulk4$SUFFIX.out
+        "
    #
    for file in $list
    do
