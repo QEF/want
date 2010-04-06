f36c33828e6e90d86a8f79f9635bba6477a9bb18
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2010-04-06 10:56:42 +0000

    eigenchannel writing fixed. test11 updated. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk/want@1274 0e440500-e291-4b27-a0db-898e6d94e59d

--- tests/test11/run.sh
+++ tests/test11/run.sh
@@ -24,10 +24,16 @@ MANUAL=" Usage
                  the wannier minimization
  wannier         perform the above cited minimization
  plot            compute WFs on real space for plotting
+
  disentangle_bulk1
  wannier_bulk1
+ dos_bulk1
+ bands_bulk1     want calculations for Pt bulk with 1 atom per cell
+
  disentangle_bulk4
- wannier_bulk4   want calculations for Pt bulk (1 and 4 atoms)
+ wannier_bulk4   
+ dos_bulk4
+ bands_bulk4     want calculations for Pt bulk with 4 atoms per cell
 
  conductor_lead1 evaluate the transmittance across the junction
                  using leads with 1 aton
@@ -40,7 +46,10 @@ MANUAL=" Usage
  current_lead1   compute the current from the results of conductor_lead1
  current_lead4   compute the current from the results of conductor_lead4
 
- want            perform all WANT calculations
+ want            all WanT calculations for the conductor region
+ want_bulk1      all WanT bulk1 calculations 
+ want_bulk4      all WanT bulk4 calculations 
+ want_all        perform all WanT calculations
  all             perform all the above described steps
 
  check           check results with the reference outputs
@@ -72,10 +81,16 @@ NSCF_BULK4=
 DISENTANGLE=
 WANNIER=
 PLOT=
+
 DISENTANGLE_BULK1=
 WANNIER_BULK1=
+DOS_BULK1=
+BANDS_BULK1=
+
 DISENTANGLE_BULK4=
 WANNIER_BULK4=
+DOS_BULK4=
+BANDS_BULK4=
 
 CONDUCTOR_BULK1=
 CONDUCTOR_BULK4=
@@ -106,23 +121,38 @@ case $INPUT in
    (disentangle)    DISENTANGLE=yes ;;
    (wannier)        WANNIER=yes ;;
    (plot)           PLOT=yes ;;
+
    (disentangle_bulk1)    DISENTANGLE_BULK1=yes ;;
    (wannier_bulk1)        WANNIER_BULK1=yes ;;
+   (dos_bulk1)            DOS_BULK1=yes ;;
+   (bands_bulk1)          BANDS_BULK1=yes ;;
+   (conductor_bulk1)      CONDUCTOR_BULK1=yes ;;
+
    (disentangle_bulk4)    DISENTANGLE_BULK4=yes ;;
    (wannier_bulk4)        WANNIER_BULK4=yes ;;
-
-   (conductor_bulk1)      CONDUCTOR_BULK1=yes ;;
+   (dos_bulk4)            DOS_BULK4=yes ;;
+   (bands_bulk4)          BANDS_BULK4=yes ;;
    (conductor_bulk4)      CONDUCTOR_BULK4=yes ;;
+
    (conductor_lead1)      CONDUCTOR_LEAD1=yes ;;
    (conductor_lead4)      CONDUCTOR_LEAD4=yes ;;
    (conductor_auto)       CONDUCTOR_AUTO=yes ;;
    (current_lead1)        CURRENT_LEAD1=yes ;;
    (current_lead4)        CURRENT_LEAD4=yes ;;
  
-   (want)           DISENTANGLE=yes ; WANNIER=yes ; PLOT=yes ;
+   (want)           DISENTANGLE=yes ; WANNIER=yes ; PLOT=yes ;;
+
+   (want_bulk1)     DISENTANGLE_BULK1=yes ; WANNIER_BULK1=yes ; 
+                    DOS_BULK1=yes ; BANDS_BULK1=yes; CONDUCTOR_BULK1=yes ;; 
+
+   (want_bulk4)     DISENTANGLE_BULK4=yes ; WANNIER_BULK4=yes ; 
+                    DOS_BULK4=yes ; BANDS_BULK4=yes; CONDUCTOR_BULK4=yes ;; 
+
+   (want_all)       DISENTANGLE=yes ; WANNIER=yes ; PLOT=yes ;
                     DISENTANGLE_BULK1=yes ; WANNIER_BULK1=yes ; 
+                    DOS_BULK1=yes ; BANDS_BULK1=yes; CONDUCTOR_BULK1=yes ; 
                     DISENTANGLE_BULK4=yes ; WANNIER_BULK4=yes ; 
-                    CONDUCTOR_BULK1=yes ; CONDUCTOR_BULK4=yes ; 
+                    DOS_BULK4=yes ; BANDS_BULK4=yes; CONDUCTOR_BULK4=yes ; 
                     CONDUCTOR_LEAD1=yes ; CONDUCTOR_LEAD4=yes ; 
                     CONDUCTOR_AUTO=yes ; CONDUCTOR_BULK=yes ; 
                     CURRENT_LEAD1=yes ; CURRENT_LEAD4=yes ;; 
@@ -132,8 +162,9 @@ case $INPUT in
                     SCF_BULK4=yes ; NSCF_BULK4=yes ;
                     DISENTANGLE=yes ; WANNIER=yes ; PLOT=yes ;
                     DISENTANGLE_BULK1=yes ; WANNIER_BULK1=yes ; 
+                    DOS_BULK1=yes ; BANDS_BULK1=yes; CONDUCTOR_BULK1=yes ; 
                     DISENTANGLE_BULK4=yes ; WANNIER_BULK4=yes ; 
-                    CONDUCTOR_BULK1=yes ; CONDUCTOR_BULK4=yes ; 
+                    DOS_BULK4=yes ; BANDS_BULK4=yes; CONDUCTOR_BULK4=yes ; 
                     CONDUCTOR_LEAD1=yes ; CONDUCTOR_LEAD4=yes ; 
                     CONDUCTOR_AUTO=yes ; CONDUCTOR_BULK=yes ; 
                     CURRENT_LEAD1=yes ; CURRENT_LEAD4=yes ;; 
@@ -221,14 +252,35 @@ run_disentangle  NAME=DISENTANGLE_BULK1  SUFFIX=${SUFFIX}_bulk1  RUN=$DISENTANGL
 run_wannier  NAME=WANNIER_BULK1  SUFFIX=${SUFFIX}_bulk1  RUN=$WANNIER_BULK1
 
 #
+# running DOS BULK1
+#
+run_dos  NAME=DOS_BULK1  SUFFIX=${SUFFIX}_bulk1  RUN=$DOS_BULK1
+
+#
+# running BANDS BULK1
+#
+run_bands  NAME=BANDS_BULK1  SUFFIX=${SUFFIX}_bulk1  RUN=$BANDS_BULK1
+
+
+#
 # running DISENTANGLE BULK4
 #
-run_disentangle  NAME=DISENTANGLE_BULK1  SUFFIX=${SUFFIX}_bulk4 RUN=$DISENTANGLE_BULK4
+run_disentangle  NAME=DISENTANGLE_BULK4  SUFFIX=${SUFFIX}_bulk4 RUN=$DISENTANGLE_BULK4
 
 #
 # running WANNIER BULK4
 #
 run_wannier  NAME=WANNIER_BULK4  SUFFIX=${SUFFIX}_bulk4  RUN=$WANNIER_BULK4
+
+#
+# running DOS BULK4
+#
+run_dos  NAME=DOS_BULK4  SUFFIX=${SUFFIX}_bulk4  RUN=$DOS_BULK4
+
+#
+# running BANDS BULK4
+#
+run_bands  NAME=BANDS_BULK4  SUFFIX=${SUFFIX}_bulk4  RUN=$BANDS_BULK4
 
 
 
@@ -236,14 +288,14 @@ run_wannier  NAME=WANNIER_BULK4  SUFFIX=${SUFFIX}_bulk4  RUN=$WANNIER_BULK4
 #
 # running CONDUCTOR BULK
 #
-run_conductor NAME=CONDUCTOR_BULK1  SUFFIX=$SUFFIX  RUN=$CONDUCTOR_BULK1
-run_conductor NAME=CONDUCTOR_BULK4  SUFFIX=$SUFFIX  RUN=$CONDUCTOR_BULK4
+run_conductor NAME=CONDUCTOR_BULK1  SUFFIX=${SUFFIX}_bulk1  RUN=$CONDUCTOR_BULK1
+run_conductor NAME=CONDUCTOR_BULK4  SUFFIX=${SUFFIX}_bulk4  RUN=$CONDUCTOR_BULK4
 
 #
 # running CONDUCTOR LEAD
 #
-run_conductor NAME=CONDUCTOR_LEAD1  SUFFIX=$SUFFIX  RUN=$CONDUCTOR_LEAD1
-run_conductor NAME=CONDUCTOR_LEAD4  SUFFIX=$SUFFIX  RUN=$CONDUCTOR_LEAD4
+run_conductor NAME=CONDUCTOR_LEAD1  SUFFIX=${SUFFIX}_lead1  RUN=$CONDUCTOR_LEAD1
+run_conductor NAME=CONDUCTOR_LEAD4  SUFFIX=${SUFFIX}_lead4  RUN=$CONDUCTOR_LEAD4
 
 #
 # running CONDUCTOR_AUTO
@@ -253,8 +305,8 @@ run_conductor NAME=CONDUCTOR_AUTO  SUFFIX=${SUFFIX}_auto  RUN=$CONDUCTOR_AUTO
 #
 # running CURRENT
 #
-run_current NAME=CURRENT_LEAD1  SUFFIX=$SUFFIX  RUN=$CURRENT_LEAD1
-run_current NAME=CURRENT_LEAD4  SUFFIX=$SUFFIX  RUN=$CURRENT_LEAD4
+run_current NAME=CURRENT_LEAD1  SUFFIX=${SUFFIX}_lead1  RUN=$CURRENT_LEAD1
+run_current NAME=CURRENT_LEAD4  SUFFIX=${SUFFIX}_lead4  RUN=$CURRENT_LEAD4
 
 
 
