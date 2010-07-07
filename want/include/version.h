c8fa8b1855e47e713e89ef7b715747768ce0bba0
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2010-07-07 14:41:48 +0000

    version upgraded to 2.4.0-alpha. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@1332 0e440500-e291-4b27-a0db-898e6d94e59d

--- want/include/version.h
+++ want/include/version.h
@@ -8,9 +8,9 @@
 ! 
 #define  __VERSION_NAME    "WanT"
 #define  __VERSION_MAJOR   "2"
-#define  __VERSION_MINOR   "3"
+#define  __VERSION_MINOR   "4"
 #define  __VERSION_PATCH   "0"
-#define  __VERSION_LABEL   ""
+#define  __VERSION_LABEL   "alpha"
 
 
 
