648510eb18686ba2ae0443fbfb31f29ac8cebcca
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2011-11-08 00:45:38 +0000

    sax2qexml utilities added. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk/want@1404 0e440500-e291-4b27-a0db-898e6d94e59d

---
+++ utility/sax2qexml.f90
@@ -0,0 +1,510 @@
+!
+! Copyright (C) 2011 Andrea Ferretti
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!=====================================================
+PROGRAM sax2qexml
+   !=====================================================
+   !
+   ! Convert STATES and GW_QP from sax fmt into a
+   ! proper qexml  prefix.save directory.
+   !
+   ! the qexml $prefix.save cirectory can be copied
+   ! into      $prefix_$calc.save   (calc=GW,HF)
+   !
+   ! eigenval.xml, evc.dat 
+   ! 
+   ! are reformatted according to the file states
+   ! or GW_QP + the existing .save directory
+   !
+   ! textual input is suggested
+   !
+   USE version_module,       ONLY : version_number
+   USE kinds,                ONLY : dbl
+   USE parameters,           ONLY : nstrx
+   USE io_module,            ONLY : stdout, stdin
+   USE io_module,            ONLY : work_dir, prefix, label => postfix
+   USE control_module,       ONLY : verbosity, debug_level, use_debug_mode
+   USE timing_module,        ONLY : timing
+   USE log_module,           ONLY : log_push, log_pop
+   USE parser_module,        ONLY : log2char, change_case
+   !
+   IMPLICIT NONE
+
+   !
+   ! input variables
+   !
+   CHARACTER(nstrx)  :: qp_file, qp_states
+
+   NAMELIST /INPUT/  prefix, work_dir, label, qp_file, qp_states
+
+   !   
+   ! end of declariations
+   !   
+
+!
+!------------------------------
+! main body
+!------------------------------
+!
+      CALL startup(version_number,'sax2qexml')
+
+      !   
+      ! read input
+      !   
+      CALL sax2qexml_input( )
+
+      !
+      ! do the main task 
+      ! 
+      CALL do_sax2qexml( prefix, work_dir, label, qp_file, qp_states )
+
+      !
+      ! finalize
+      !
+      CALL shutdown( 'sax2qexml' )
+
+CONTAINS
+
+
+!********************************************************
+   SUBROUTINE sax2qexml_input()
+   !********************************************************
+   !
+   ! Read INPUT namelist from stdin
+   !
+   USE mp,            ONLY : mp_bcast
+   USE io_module,     ONLY : io_init, ionode, ionode_id
+   !
+   IMPLICIT NONE
+
+      CHARACTER(15)    :: subname = 'sax2qexml_input'
+      INTEGER          :: ierr
+      !
+      ! end of declarations
+      !
+
+      CALL timing( subname, OPR='start' )
+
+      !
+      ! init input namelist
+      !
+      prefix                      = ' '
+      work_dir                    = ' '
+      label                       = ' '
+      qp_file                     = ' '
+      qp_states                   = ' '
+
+      CALL input_from_file ( stdin )
+      !
+      IF (ionode) READ(stdin, INPUT, IOSTAT=ierr)
+      !
+      CALL mp_bcast( ierr, ionode_id )
+      IF ( ierr /= 0 )  CALL errore(subname,'Unable to read namelist INPUT',ABS(ierr))
+
+      !
+      ! broadcast
+      !
+      CALL mp_bcast( prefix,            ionode_id )
+      CALL mp_bcast( work_dir,          ionode_id )
+      CALL mp_bcast( label,             ionode_id )
+      CALL mp_bcast( qp_file,           ionode_id )
+      CALL mp_bcast( qp_states,         ionode_id )
+
+      !
+      ! summary
+      !
+      IF ( ionode ) THEN
+          !   
+          CALL write_header( stdout, "INPUT Summary" )
+          !   
+          WRITE( stdout, "(   7x,'              work dir :',5x,   a)") TRIM(work_dir)
+          WRITE( stdout, "(   7x,'                prefix :',5x,   a)") TRIM(prefix)
+          WRITE( stdout, "(   7x,'                 label :',5x,   a)") TRIM(label)
+          WRITE( stdout, "(   7x,'               qp_file :',5x,   a)") TRIM(qp_file)
+          WRITE( stdout, "(   7x,'             qp_states :',5x,   a)") TRIM(qp_states)
+          !
+      ENDIF
+      !
+      CALL timing(subname,OPR='stop')
+      !
+      RETURN
+      !
+   END SUBROUTINE sax2qexml_input
+
+
+!************************************************************************
+   SUBROUTINE do_sax2qexml( prefix, work_dir, label, qp_file, qp_states )
+   !************************************************************************
+   !
+   ! performs the main task
+   !
+   USE kinds
+   USE constants,            ONLY : ZERO, CZERO
+   USE parameters,           ONLY : nstrx
+   USE io_module,            ONLY : ionode, ionode_id, &
+                                    dft_unit, sax_unit => aux1_unit, &
+                                    qexml_unit => aux2_unit
+   USE mp,                   ONLY : mp_bcast
+   USE util_module,          ONLY : mat_mul
+   USE qexml_module
+   USE iotk_module
+   !
+   IMPLICIT NONE
+      !
+      ! input vars
+      !
+      CHARACTER(*),  INTENT(IN) :: prefix, work_dir
+      CHARACTER(*),  INTENT(IN) :: label
+      CHARACTER(*),  INTENT(IN) :: qp_file
+      CHARACTER(*),  INTENT(IN) :: qp_states
+  
+      !
+      ! local vars
+      !
+      CHARACTER(12) :: subname="do_sax2qexml"
+      !
+      CHARACTER(256):: filename 
+      CHARACTER(256):: dirname_in, dirname_out
+      CHARACTER(256):: attr
+      CHARACTER(15) :: str
+      !
+      INTEGER       :: ib, ik, isp, ig
+      INTEGER       :: nbnd, nkpts, nspin, npw
+      INTEGER       :: ibnd_min, ibnd_max
+      INTEGER       :: npwkx
+      LOGICAL       :: lhave_qp_states
+      LOGICAL       :: lgamma_only
+      !
+      INTEGER,      ALLOCATABLE :: npwk(:)
+      REAL(dbl),    ALLOCATABLE :: eig(:,:,:)
+      COMPLEX(dbl), ALLOCATABLE :: U_rot(:,:,:,:)
+      COMPLEX(dbl), ALLOCATABLE :: wfc(:,:)
+      COMPLEX(dbl), ALLOCATABLE :: wfc_new(:,:)
+      !
+      INTEGER       :: ierr
+
+      !   
+      ! end of declarations
+      !   
+
+!
+!------------------------------
+! main body
+!------------------------------
+!
+
+      CALL timing(subname,OPR='start')
+      !CALL log_push(subname)
+
+      !
+      ! preliminaries
+      !
+      lhave_qp_states = ( LEN_TRIM( qp_states ) /= 0 )
+      !
+      dirname_in   = TRIM(work_dir) // "/" // TRIM( prefix ) // ".save/" 
+      dirname_out  = TRIM(work_dir) // "/" // TRIM( prefix ) // "_" // TRIM(label) // ".save/" 
+      filename     = TRIM( dirname_in ) // "data-file.xml"
+      !
+      CALL qexml_init( UNIT_IN=dft_unit,         UNIT_OUT=qexml_unit, &
+                       DIR_IN=TRIM(dirname_in),  DIR_OUT=TRIM(dirname_out) )
+
+
+      !
+      ! open prefix.save and get main dimensions
+      ! nbnd, nkpts, npwk (check others)
+      !
+      !
+      IF (ionode) WRITE(stdout,"(/,2x,'Reading from dftdata file...')")
+      !
+      CALL qexml_openfile( filename, "read", IERR=ierr )
+      IF ( ierr/=0) CALL errore(subname,'opening dftdata file: '//TRIM(filename),ABS(ierr))
+      !
+      IF ( ionode ) THEN
+          !
+          CALL qexml_read_bands_info( NBND=nbnd, NUM_K_POINTS=nkpts, &
+                                      NSPIN=nspin, IERR=ierr )
+          !
+      ENDIF
+      !
+      CALL mp_bcast( nbnd,    ionode_id )
+      CALL mp_bcast( nkpts,   ionode_id )
+      CALL mp_bcast( nspin,   ionode_id )
+      CALL mp_bcast( ierr,    ionode_id )
+      !
+      IF ( ierr/=0 ) CALL errore(subname,"reading bands_info",ABS(ierr))
+      !
+      !
+      IF ( nspin /= 1 ) CALL errore(subname,"nspin/=1 not implemented",10)
+      !
+      ALLOCATE( npwk(nkpts), STAT=ierr )
+      IF ( ierr/=0 ) CALL errore(subname,"allocating npwk",ABS(ierr))
+      !
+      IF ( ionode ) THEN
+          !
+          DO ik = 1, nkpts
+              CALL qexml_read_gk(ik, NPWK=npwk(ik), GAMMA_ONLY=lgamma_only, IERR=ierr)
+          ENDDO
+          !
+      ENDIF
+      !
+      CALL mp_bcast( npwk,        ionode_id)
+      CALL mp_bcast( lgamma_only, ionode_id)
+      !
+      npwkx = MAXVAL( npwk(1:nkpts) )
+      !
+      IF ( lgamma_only ) CALL errore(subname,"gamma_only not implemented",10)
+      !
+      !
+      CALL qexml_closefile( "read", IERR=ierr)
+      IF ( ierr/=0 ) CALL errore(subname,"closing dft datafile", ABS(ierr))
+  
+
+      !
+      ! open qp_file, read eigenvalues & eigenvector matrix
+      ! dump eigenval.xml files
+      !
+      IF (ionode) WRITE(stdout,"(2x,'Reading from qp_file...')")
+      !
+      CALL iotk_open_read( sax_unit, FILE=TRIM(qp_file), IERR=ierr )
+      IF ( ierr/=0 ) CALL errore(subname,"opening qp_file: "//TRIM(qp_file),ABS(ierr))
+      !
+      str=TRIM(label)//"_QP"
+      !
+      CALL iotk_scan_begin(sax_unit, TRIM(str), IERR=ierr)
+      IF ( ierr/=0 ) CALL errore(subname,"scan begin "//TRIM(str),ABS(ierr))
+      !
+      CALL iotk_scan_dat(sax_unit, "nbmin", ibnd_min, IERR=ierr)
+      IF ( ierr/=0 ) CALL errore(subname,"scan nbmin",ABS(ierr))
+      CALL iotk_scan_dat(sax_unit, "nbmax", ibnd_max, IERR=ierr)
+      IF ( ierr/=0 ) CALL errore(subname,"scan nbmax",ABS(ierr))
+      !
+      ! checks
+      !
+      IF ( ibnd_min /= 1 )   CALL errore(subname,"ibnd_min /= 1", 10)
+      IF ( ibnd_max > nbnd ) CALL errore(subname,"ibnd_max too large", 10)
+      !
+      ALLOCATE( eig(nbnd,nkpts,nspin), STAT=ierr )
+      IF ( ierr/=0 ) CALL errore(subname,"allocating eig",ABS(ierr))
+      !
+      DO isp = 1, nspin
+      DO ik  = 1, nkpts
+          !
+          eig(1:ibnd_min-1,ik,isp)    = ZERO
+          eig(ibnd_max+1:nbnd,ik,isp) = ZERO
+          !
+          WRITE(str,"('energies',i3.3)") ik + (isp-1)*nspin
+          CALL iotk_scan_dat( sax_unit, TRIM(str), eig(ibnd_min:ibnd_max,ik,isp), IERR=ierr )
+          IF ( ierr/=0 ) CALL errore(subname,"searching for "//TRIM(str), ABS(ierr))
+          !
+      ENDDO
+      ENDDO
+      ! 
+      ALLOCATE( U_rot(ibnd_min:ibnd_max,ibnd_min:ibnd_max,nkpts,nspin), STAT=ierr )
+      IF ( ierr/=0 ) CALL errore(subname,"allocating U_rot",ABS(ierr))
+      ! 
+      DO isp = 1, nspin
+      DO ik  = 1, nkpts
+          !
+          WRITE(str,"('eigenvec',i3.3)") ik + (isp-1)*nspin
+          CALL iotk_scan_dat( sax_unit, TRIM(str), U_rot(:,:,ik,isp), IERR=ierr )
+          IF ( ierr/=0 ) CALL errore(subname,"searching for "//TRIM(str), ABS(ierr))
+          !
+      ENDDO
+      ENDDO
+      ! 
+      str=TRIM(label)//"_QP"
+      !
+      CALL iotk_scan_end(sax_unit, TRIM(str), IERR=ierr)
+      IF ( ierr/=0 ) CALL errore(subname,"scan end "//TRIM(str),ABS(ierr))
+      !
+      CALL iotk_close_read( sax_unit, IERR=ierr )
+      IF ( ierr/=0 ) CALL errore(subname,"closing qp_file: "//TRIM(qp_file),ABS(ierr))
+
+      !
+      ! dump eigenval.xml files
+      !
+      ! Units=Hartree, no conversion needed
+      !eig(:,:,:) = eig(:,:,:) * TWO * RYD
+      !
+      IF ( ionode ) WRITE(stdout,"(2x,'Write eigenval to file...',/)")
+      !
+      IF ( ionode ) THEN
+          !
+          DO isp = 1, nspin
+          DO ik  = 1, nkpts
+              !
+              IF ( nspin == 1 ) THEN
+                  CALL qexml_write_bands( ik, NBND=nbnd,  &
+                                          EIG=eig(:,ik,isp), ENERGY_UNITS="Hartree" )
+              ELSE
+                  CALL qexml_write_bands( ik, ISPIN=isp, NBND=nbnd, &
+                                          EIG=eig(:,ik,isp), ENERGY_UNITS="Hartree" )
+              ENDIF
+              !
+          ENDDO
+          ENDDO
+          !
+      ENDIF
+      ! 
+      DEALLOCATE( eig, STAT=ierr)
+      IF ( ierr/=0 ) CALL errore(subname,"deallocating eig",ABS(ierr))
+      
+
+      !
+      ! if have qp_states, read wfcs and dump them
+      ! otherwise, read wfcs from $prefix.save
+      ! and rotate them according to the eigenvector matrices
+      !
+      IF ( .NOT. lhave_qp_states ) THEN
+          ALLOCATE( wfc(npwkx,nbnd), STAT=ierr )
+          IF( ierr/=0 ) CALL errore(subname,"allocating wfc",ABS(ierr))
+      ENDIF
+      !
+      ALLOCATE( wfc_new(npwkx,nbnd), STAT=ierr )
+      IF( ierr/=0 ) CALL errore(subname,"allocating wfc_new",ABS(ierr))
+
+      !
+      ! init read
+      !
+      IF ( lhave_qp_states ) THEN
+          !
+          CALL iotk_open_read( sax_unit, FILE=TRIM(qp_states), IERR=ierr )
+          IF ( ierr/=0 ) CALL errore(subname,"opening qp_states: "//TRIM(qp_states),ABS(ierr))
+          !
+          str=TRIM(label)//"_states"
+          !
+          CALL iotk_scan_begin(sax_unit, TRIM(str), IERR=ierr)
+          IF ( ierr/=0 ) CALL errore(subname,"scan begin "//TRIM(str),ABS(ierr))
+          !
+      ENDIF
+      
+      !
+      ! read and/or convert wfc data
+      !
+      DO isp = 1, nspin
+      DO ik = 1, nkpts
+          !
+          IF ( ionode ) WRITE(stdout,"(2x,'Read/Write wfc [',i3,' ] to file...')") ik
+          !
+          have_qp_states_if :&
+          IF ( lhave_qp_states ) THEN
+              !
+              wfc_new(:,1:ibnd_min-1)    = CZERO
+              wfc_new(:,ibnd_max+1:nbnd) = CZERO
+              !
+              DO ib = ibnd_min, ibnd_max
+                  !
+                  str="wfc"//TRIM(iotk_index(ik))//TRIM(iotk_index(ib))
+                  !
+                  CALL iotk_scan_begin(sax_unit, TRIM(str), IERR=ierr)
+                  IF ( ierr/=0 ) CALL errore(subname,"scan begin "//TRIM(str),ABS(ierr))
+                  !
+                  CALL iotk_scan_empty(sax_unit, "info", ATTR=attr, IERR=ierr)
+                  IF ( ierr/=0 ) CALL errore(subname,"scan info",ABS(ierr))
+                  CALL iotk_scan_attr(attr, "npw", npw, IERR=ierr)
+                  IF ( ierr/=0 ) CALL errore(subname,"scan npw",ABS(ierr))
+                  !
+                  IF ( npw /= npwk(ik) ) CALL errore(subname,"unexpected npw",10)
+                  !
+                  CALL iotk_scan_dat(sax_unit, "val", wfc_new(:,ib), IERR=ierr)
+                  IF ( ierr/=0 ) CALL errore(subname,"scan end "//TRIM(str),ABS(ierr))
+                  !
+                  CALL iotk_scan_end(sax_unit, TRIM(str), IERR=ierr)
+                  IF ( ierr/=0 ) CALL errore(subname,"scan end "//TRIM(str),ABS(ierr))
+                  !
+              ENDDO
+              !
+          ELSE
+              !
+              IF ( nspin == 1 ) THEN
+                   !
+                   CALL qexml_read_wfc( 1, nbnd, ik, &
+                                        WF=wfc(:,:), IERR=ierr )
+                   !
+              ELSE
+                   !
+                   CALL qexml_read_wfc( 1, nbnd, ik, ISPIN=isp, &
+                                        WF=wfc(:,:), IERR=ierr )
+                   !
+              ENDIF
+              !
+              IF ( ierr/=0 ) CALL errore(subname,"reading old wfcs",ABS(ierr))
+              !
+              CALL mat_mul( wfc_new, wfc, 'N', U_rot(:,:,ik,isp), 'N', &
+                            npwk(ik), SIZE(U_rot,1), SIZE(U_rot,1) )
+              !
+          ENDIF have_qp_states_if
+
+          !
+          ! dump evc.dat
+          !
+          IF ( ionode ) THEN
+              !
+              IF ( nspin == 1 ) THEN
+                  !
+                  CALL qexml_write_wfc( nbnd, nkpts, nspin, ik, &
+                              NGW=0, IGWX=npwk(ik), GAMMA_ONLY=lgamma_only, &
+                              WF=wfc(1:npwk(ik),:) )
+                  !
+              ELSE
+                  !
+                  CALL qexml_write_wfc( nbnd, nkpts, nspin, ik, ISPIN=isp, &
+                              NGW=0, IGWX=npwk(ik), GAMMA_ONLY=lgamma_only, &
+                              WF=wfc(1:npwk(ik),:) )
+                  !
+              ENDIF
+              !
+          ENDIF
+          !
+      ENDDO
+      ENDDO
+      !
+      !
+      IF ( lhave_qp_states ) THEN
+          !
+          str=TRIM(label)//"_states"
+          !
+          CALL iotk_scan_end(sax_unit, TRIM(str), IERR=ierr)
+          IF ( ierr/=0 ) CALL errore(subname,"scan end "//TRIM(str),ABS(ierr))
+          !
+          CALL iotk_close_read( sax_unit, IERR=ierr )
+          IF ( ierr/=0 ) CALL errore(subname,"closing qp_states: "//TRIM(qp_states),ABS(ierr))
+          !
+      ENDIF
+
+
+      !
+      ! cleanup
+      !
+      DEALLOCATE( npwk, STAT=ierr )
+      IF ( ierr/=0 ) CALL errore(subname, 'deallocating npwk', ABS(ierr))
+      !
+      IF ( ALLOCATED( wfc ) ) THEN
+          DEALLOCATE( wfc, STAT=ierr )
+          IF ( ierr/=0 ) CALL errore(subname, 'deallocating wfc', ABS(ierr))
+      ENDIF
+      !
+      DEALLOCATE( wfc_new, STAT=ierr )
+      IF ( ierr/=0 ) CALL errore(subname, 'deallocating wfc_new', ABS(ierr))
+      !
+      IF ( ALLOCATED(U_rot) ) THEN
+          DEALLOCATE( U_rot, STAT=ierr )
+          IF ( ierr/=0 ) CALL errore(subname,"deallocating U_rot", ABS(ierr))
+      ENDIF
+      !   
+      !
+      CALL timing(subname,OPR='stop')
+      !CALL log_pop(subname)
+      !
+      RETURN
+      !
+   END SUBROUTINE do_sax2qexml
+
+
+END PROGRAM sax2qexml
+
+
