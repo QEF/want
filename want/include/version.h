9e7e686722a3e1e60c8b744ec91173a365f2c78c
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2012-05-29 14:58:05 +0000

    version changed to want-2.3.1. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/branches/WanT-2_3-branch@1448 0e440500-e291-4b27-a0db-898e6d94e59d

--- want/include/version.h
+++ want/include/version.h
@@ -9,7 +9,7 @@
 #define  __VERSION_NAME    "WanT"
 #define  __VERSION_MAJOR   "2"
 #define  __VERSION_MINOR   "3"
-#define  __VERSION_PATCH   "0"
+#define  __VERSION_PATCH   "1"
 #define  __VERSION_LABEL   ""
 
 
