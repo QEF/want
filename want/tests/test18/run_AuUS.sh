6dbfc842b84cc62365f1db5d73da01ee5797c18d
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2012-12-04 22:37:49 +0000

    test updated. gold chain added. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@1550 0e440500-e291-4b27-a0db-898e6d94e59d

---
+++ want/tests/test18/run_AuUS.sh
@@ -0,0 +1,151 @@
+#! /bin/bash
+#
+# Unfolding of the band structure of a Au chain (USPP, gamma)
+#
+#================================================================
+#
+# Input flags for this script (./run.sh FLAG): 
+#
+MANUAL=" Usage
+   run.sh [FLAG]
+
+ where FLAG is one of the following 
+ (no FLAG will print this manual page) :
+ 
+ scf             DFT self-consistent calculation
+ nscf            DFT inon-self-consistent calculation
+ dft             perform SCF + NSCF 
+ disentangle     matrix elements and disentanglement
+ wannier         wannierization
+
+ unfold          compute translations and unfold the real space Hamiltonian
+ bands           interpolate the band structure
+ want            perform DISENTANGLE, WANNIER, UNFOLD, BANDS
+ all             perform all the above described steps
+
+ check           check results with the reference outputs
+ clean           delete all output files and the temporary directory
+"
+#
+#================================================================
+#
+
+#
+# source common enviroment
+. ../environment.conf
+#
+# source low level macros for test
+. ../../script/libtest.sh
+
+#
+# macros
+SUFFIX="_AuUS"
+
+#
+# evaluate the starting choice about what is to run 
+
+SCF=
+NSCF=
+DISENTANGLE=
+WANNIER=
+UNFOLD=
+BANDS=
+CHECK=
+CLEAN=
+
+if [ $# = 0 ] ; then echo "$MANUAL" ; exit 0 ; fi
+INPUT=`echo $1 | tr [:upper:] [:lower:]`
+
+case $INPUT in 
+   (scf)            SCF=yes ;;
+   (nscf)           NSCF=yes ;;
+   (dft)            SCF=yes ;;
+   (disentangle)    DISENTANGLE=yes ;;
+   (wannier)        WANNIER=yes ;;
+   (unfold)         UNFOLD=yes ;;
+   (bands)          BANDS=yes ;;
+   (want)           DISENTANGLE=yes ; WANNIER=yes ;
+                    UNFOLD=yes ; BANDS=yes ;;
+   (all)            SCF=yes ; NSCF=yes ;
+                    DISENTANGLE=yes ; WANNIER=yes ;
+                    UNFOLD=yes ; BANDS=yes ;; 
+   (check)          CHECK=yes ;;
+   (clean)          CLEAN=yes ;;
+   (*)              echo " Invalid input FLAG, type ./run.sh for help" ; exit 1 ;;
+esac
+
+#
+# switches
+#
+if [ "$PLOT_SWITCH" = "no" ] ; then PLOT=".FALSE." ; fi
+
+
+#
+# initialize 
+#
+if [ -z "$CLEAN" ] ; then
+   test_init 
+fi
+#
+
+
+#-----------------------------------------------------------------------------
+
+#
+# running DFT SCF
+#
+run_dft  NAME=SCF   SUFFIX=$SUFFIX  RUN=$SCF
+
+#
+# running DFT NSCF
+#
+run_dft  NAME=NSCF   SUFFIX=$SUFFIX  RUN=$NSCF
+
+#
+# running DISENTANGLE
+#
+run_disentangle  SUFFIX=$SUFFIX  RUN=$DISENTANGLE
+
+#
+# running WANNIER
+#
+run_wannier  SUFFIX=$SUFFIX  RUN=$WANNIER
+
+#
+# running UNFOLD
+#
+run_unfold  SUFFIX=$SUFFIX  RUN=$BANDS
+
+#
+# running BANDS
+#
+run_bands  SUFFIX=$SUFFIX  RUN=$BANDS
+
+
+#
+# running CHECK
+#
+if [ "$CHECK" = yes ] ; then
+   echo "running CHECK... "
+   #
+   cd $TEST_HOME
+   list="disentangle$SUFFIX.out wannier$SUFFIX.out"
+   #
+   for file in $list
+   do
+      ../../script/check.sh $file
+   done
+fi
+
+
+#
+# eventually clean
+#
+run_clean  RUN=$CLEAN
+
+
+#
+# exiting
+exit 0
+
+
