b9b743580a36d70375ed96aa9be244d4d273d8fd
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2012-12-04 22:38:28 +0000

    test18 added. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk/want@1552 0e440500-e291-4b27-a0db-898e6d94e59d

--- script/libtest.sh
+++ script/libtest.sh
@@ -479,6 +479,39 @@ run_blc2wan () {
 
 
 #
+#----------------------
+run_unfold () {
+#----------------------
+#
+   local NAME=UNFOLD
+   local EXEC=$WANT_BIN/unfold.x
+   local RUN=yes
+   local INPUT=
+   local OUTPUT=
+   local SUFFIX=
+   local PARALLEL=yes
+   local name_tmp
+   
+   for arg 
+   do
+         [[ "$arg" == NAME=* ]]      && NAME="${arg#NAME=}"
+         [[ "$arg" == INPUT=* ]]     && INPUT="${arg#INPUT=}"
+         [[ "$arg" == OUTPUT=* ]]    && OUTPUT="${arg#OUTPUT=}"
+         [[ "$arg" == SUFFIX=* ]]    && SUFFIX="${arg#SUFFIX=}"
+         [[ "$arg" == RUN=* ]]       && RUN="${arg#RUN=}"
+         [[ "$arg" == PARALLEL=* ]]  && PARALLEL="${arg#PARALLEL=}"
+   done
+   
+   [[ "$RUN" != "yes" ]]  && return
+
+   name_tmp=`echo $NAME | tr [:upper:] [:lower:]`
+   if [ -z "$INPUT" ]  ; then  INPUT=$TEST_HOME/unfold$SUFFIX.in  ; fi
+   if [ -z "$OUTPUT" ] ; then OUTPUT=$TEST_HOME/unfold$SUFFIX.out ; fi
+
+   run NAME=$NAME INPUT=$INPUT OUTPUT=$OUTPUT EXEC=$EXEC PARALLEL=$PARALLEL
+}
+
+
 #
 #----------------------
 run_plot () {
