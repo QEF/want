b57ba7a2c671b0e400265f07820c32b263933678
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2013-06-15 14:58:07 +0000

    atmproj tools updated. AF
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@1604 0e440500-e291-4b27-a0db-898e6d94e59d

--- want/src/transport/datafiles.f90
+++ want/src/transport/datafiles.f90
@@ -175,7 +175,7 @@ CONTAINS
                !
            CASE( 'atmproj' )
                !
-               CALL atmproj_to_internal( TRIM(filename), TRIM(filename)//'.ham', 'hamiltonian', do_orthoovp )
+               CALL atmproj_to_internal( TRIM(filename), FILEHAM=TRIM(filename)//'.ham', DO_ORTHOOVP=do_orthoovp )
                !
                WRITE( stdout, "(2x, A,' converted from ATMPROJ to internal fmt' )") &
                    TRIM( filename )
