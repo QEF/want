4d3f1257b360bf44ed59f4eeda6f3b2464586368
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2014-04-18 11:47:00 +0000

    version updated to 2.6.0-devel
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk@1672 0e440500-e291-4b27-a0db-898e6d94e59d

--- want/include/version.h
+++ want/include/version.h
@@ -8,9 +8,9 @@
 ! 
 #define  __VERSION_NAME    "WanT"
 #define  __VERSION_MAJOR   "2"
-#define  __VERSION_MINOR   "5"
+#define  __VERSION_MINOR   "6"
 #define  __VERSION_PATCH   "0"
-#define  __VERSION_LABEL   " "
+#define  __VERSION_LABEL   "devel"
 
 
 
