27ee3ff9942ef1c74f5b2ac14b572b561ab95e51
ferretti <ferretti@0e440500-e291-4b27-a0db-898e6d94e59d>
2017-08-25 13:08:12 +0000

    HF output file now supported
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/want/trunk/want@1737 0e440500-e291-4b27-a0db-898e6d94e59d

--- src/wannier/read_fileqp.f90
+++ src/wannier/read_fileqp.f90
@@ -12,8 +12,9 @@
     !
     ! eigs are updated
     !
-    USE kinds,        ONLY : dbl
-    USE iotk_module,  ONLY : iotk_free_unit
+    USE kinds,         ONLY : dbl
+    USE iotk_module,   ONLY : iotk_free_unit
+    USE parser_module, ONLY : matches
     IMPLICIT NONE
     !
     ! input vars
@@ -28,9 +29,10 @@
     CHARACTER(256) :: line
     INTEGER   :: ierr, iunit, nline, NF
     INTEGER   :: ib, ik, isp, i
+    LOGICAL   :: file_is_HF,file_is_QP
     LOGICAL   :: detect_NF
-    REAL(dbl) :: rb, rk, rsp
-    REAL(dbl) :: E0, DE_QP, sgmc
+    REAL(dbl) :: rb, rk, rsp, rtmp
+    REAL(dbl) :: E0, E_QP, DE_QP, sgmc
 
 !
 !=========================
@@ -42,6 +44,14 @@
     !
     OPEN( iunit, FILE=fileqp, STATUS="old", IOSTAT=ierr)
     IF ( ierr/=0 ) CALL errore(subname,"opening "//TRIM(fileqp), ABS(ierr))
+    !
+    ! detect extension
+    !
+    file_is_QP=matches(".qp_@__@",trim(fileqp)//"_@__@")
+    file_is_HF=matches(".hf_@__@",trim(fileqp)//"_@__@")
+    !
+    if (.not.file_is_HF .and. .not. file_is_QP) &
+       CALL errore(subname,"unknown extension of fileqp = "//TRIM(fileqp),10)
     !
     detect_NF=.TRUE.
     NF=0
@@ -67,7 +77,7 @@
            line_scan:&
            DO WHILE (.TRUE.)
               NF=NF+1
-              READ(line,*,IOSTAT=ierr) (rsp, i=1,NF)  
+              READ(line,*,IOSTAT=ierr) (rtmp, i=1,NF)  
               IF (ierr/=0) EXIT line_scan
            ENDDO line_scan
            !
@@ -81,13 +91,18 @@
         !
         ! actual read-in
         !
-        IF ( NF == 6 ) THEN
+        IF ( file_is_QP .and. NF == 6 ) THEN
            READ(line,*,IOSTAT=ierr) rk, rb, E0, DE_QP, sgmc, rsp
            IF ( ierr/=0 ) CALL errore(subname,"invalid line fmt I",nline)
-        ELSE IF ( NF == 5 ) THEN
+        ELSE IF ( file_is_QP .and. NF == 5 ) THEN
            READ(line,*,IOSTAT=ierr) rk, rb, E0, DE_QP, sgmc
            IF ( ierr/=0 ) CALL errore(subname,"invalid line fmt II",nline)
            rsp=1
+        ELSE IF ( file_is_HF .and. NF == 6 ) THEN
+           READ(line,*,IOSTAT=ierr) rk, rb, E0, E_QP, rtmp, rtmp
+           IF ( ierr/=0 ) CALL errore(subname,"invalid line fmt III",nline)
+           rsp=1
+           DE_QP=E_QP-E0
         ELSE 
            CALL errore(subname,"fmt not supported",10)
         ENDIF
@@ -101,7 +116,6 @@
         IF ( ik < 0 .OR. ik > ndim2 ) CALL errore(subname,"invalid nkpts",ik)
         IF ( isp < 0 .OR. isp > ndim3 ) CALL errore(subname,"invalid nspin",isp)
         !
-        !eig(ib,ik,isp) = eig(ib,ik,isp) + DE_QP
         eig(ib,ik,isp) = E0 + DE_QP
         !
     ENDDO loop_scan
